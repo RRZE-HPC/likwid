7ae55f985ebc89e1d11d210050896621e7521877
Thomas.Roehl <TomTheBear@users.noreply.github.com>
2014-07-30 15:28:03 +0000

    v3.1: RDPMC support in msr.c

--- src/msr.c
+++ src/msr.c
@@ -60,9 +60,62 @@
 /* #####   VARIABLES  -  LOCAL TO THIS SOURCE FILE   ###################### */
 static int FD[MAX_NUM_THREADS];
 static int socket_fd = -1;
+static int rdpmc_works = 0;
 
 /* #####   FUNCTION DEFINITIONS  -  LOCAL TO THIS SOURCE FILE   ########### */
+static inline int __rdpmc(int counter, uint64_t* value)
+{
+    unsigned low, high;
+    __asm__ volatile("rdpmc" : "=a" (low), "=d" (high) : "c" (counter));
+    *value = ((low) | ((uint64_t )(high) << 32));
+    return 0;
+}
+//Needed for rdpmc check
+void segfault_sigaction(int signal, siginfo_t *si, void *arg)
+{
+    exit(1);
+}
 
+int test_rdpmc(int flag)
+{
+    int ret, waiting;
+    int pid;
+    int status = 0;
+    uint64_t tmp;
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sigaction));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = segfault_sigaction;
+    sa.sa_flags   = SA_SIGINFO;
+    
+    
+    pid = fork();
+    
+    if (pid < 0)
+    {
+        return -1;
+    }
+    if (!pid)
+    {
+        sigaction(SIGSEGV, &sa, NULL);
+        if (flag == 0)
+        {
+            __rdpmc(0, &tmp);
+        }
+        exit(0);
+    } else {
+    
+        waiting = waitpid(pid, &status, 0);
+        if (waiting < 0 || status)
+        {
+            ret = 0;
+        } else 
+        {
+            ret = 1;
+        }
+    }
+    return ret;
+}
 
 /* #####   FUNCTION DEFINITIONS  -  EXPORTED FUNCTIONS   ################## */
 
@@ -72,44 +125,37 @@ msr_init(int initSocket_fd)
 {
     if (accessClient_mode == DAEMON_AM_DIRECT)
     {
-
-        int  fd;
+        char* msr_file_name = (char*) malloc(MAX_LENGTH_MSR_DEV_NAME * sizeof(char));
 #ifdef __MIC
-        char* msr_file_name = "/dev/msr0";
-		if( access( msr_file_name, F_OK ) == -1 )
-		{
-			msr_file_name = "/dev/cpu/0/msr";
-		}
-#else 
-        char* msr_file_name = "/dev/cpu/0/msr";
-#endif 
-
-        fd = open(msr_file_name, O_RDWR);
-
-        if (fd < 0)
+        sprintf(msr_file_name,"/dev/msr0");
+        if( access( msr_file_name, F_OK ) == -1 )
         {
-            fprintf(stderr, "ERROR\n");
-            fprintf(stderr, "rdmsr: failed to open '%s': %s!\n",
-                    msr_file_name , strerror(errno));
-            fprintf(stderr, "       Please check if the msr module \
-                    is loaded and the device file has correct permissions.\n");
-            fprintf(stderr, "       Alternatively you might want to \
-                    look into (sys)daemonmode.\n\n");
+            sprintf(msr_file_name,"/dev/cpu/0/msr");
+        }
+#else 
+        sprintf(msr_file_name,"/dev/cpu/0/msr");
+#endif 
+        rdpmc_works = test_rdpmc(0);
+        
+        if (access(msr_file_name, R_OK|W_OK))
+        {
+            ERROR_PRINT("Cannot access MSR device file %s: %s.\n"
+                        "Please check if 'msr' module is loaded and device files have correct permissions\n"
+                        "Alternatively you might want to look into (sys)daemonmode\n",msr_file_name , strerror(errno));
+            free(msr_file_name);
             exit(127);
         }
-
-        close(fd);
 
         /* NOTICE: This assumes consecutive processor Ids! */
         for ( uint32_t i=0; i < cpuid_topology.numHWThreads; i++ )
         {
-            char* msr_file_name = (char*) malloc(MAX_LENGTH_MSR_DEV_NAME * sizeof(char));
+            
 #ifdef __MIC
-			sprintf(msr_file_name,"/dev/msr%d",i);
-			if( access( msr_file_name, F_OK ) == -1 )
-			{
-				sprintf(msr_file_name,"/dev/cpu/%d/msr",i);
-			}
+            sprintf(msr_file_name,"/dev/msr%d",i);
+            if( access( msr_file_name, F_OK ) == -1 )
+            {
+                sprintf(msr_file_name,"/dev/cpu/%d/msr",i);
+            }
 #else
             sprintf(msr_file_name,"/dev/cpu/%d/msr",i);
 #endif
@@ -118,9 +164,13 @@ msr_init(int initSocket_fd)
 
             if ( FD[i] < 0 )
             {
+                ERROR_PRINT("Cannot access MSR device file %s: %s\n",
+                                msr_file_name , strerror(errno));
+                free(msr_file_name);
                 ERROR;
             }
         }
+        free(msr_file_name);
     }
     else
     {
@@ -137,6 +187,7 @@ msr_finalize(void)
         {
             close(FD[i]);
         }
+        rdpmc_works = 0;
     }
     else
     {
@@ -152,9 +203,21 @@ msr_tread(const int tsocket_fd, const int cpu, uint32_t reg)
     {
         uint64_t data;
 
-        if ( pread(FD[cpu], &data, sizeof(data), reg) != sizeof(data) )
+        if (rdpmc_works && reg >= MSR_PMC0 && reg <=MSR_PMC3)
         {
-            ERROR_PRINT("cpu %d reg %x",cpu, reg);
+            if (__rdpmc(reg - MSR_PMC0, &data) )
+            {
+                ERROR_PRINT("Cannot read MSR reg 0x%x with RDPMC instruction on CPU %d\n",
+                        reg,cpu);
+            }
+        }
+        else
+        {
+            if ( pread(FD[cpu], &data, sizeof(data), reg) != sizeof(data) )
+            {
+                ERROR_PRINT("Cannot read MSR reg 0x%x with RDMSR instruction on CPU %d\n",
+                        reg, cpu);
+            }
         }
 
         return data;
@@ -173,7 +236,8 @@ msr_twrite(const int tsocket_fd, const int cpu, uint32_t reg, uint64_t data)
     {
         if (pwrite(FD[cpu], &data, sizeof(data), reg) != sizeof(data))
         {
-            ERROR_PRINT("cpu %d reg %x",cpu, reg);
+            ERROR_PRINT("Cannot write MSR reg 0x%x with WRMSR instruction on CPU %d\n",
+                        reg, cpu);
         }
     }
     else
@@ -190,9 +254,21 @@ msr_read( const int cpu, uint32_t reg)
     {
         uint64_t data;
 
-        if ( pread(FD[cpu], &data, sizeof(data), reg) != sizeof(data) )
+        if (rdpmc_works && reg >= MSR_PMC0 && reg <=MSR_PMC3)
         {
-            ERROR_PRINT("cpu %d reg %x",cpu, reg);
+            if (__rdpmc(reg - MSR_PMC0, &data) )
+            {
+                ERROR_PRINT("Cannot read MSR reg 0x%x with RDPMC instruction on CPU %d\n",
+                        reg,cpu);
+            }
+        }
+        else
+        {
+            if ( pread(FD[cpu], &data, sizeof(data), reg) != sizeof(data) )
+            {
+                ERROR_PRINT("Cannot read MSR reg 0x%x with RDMSR instruction on CPU %d\n",
+                        reg, cpu);
+            }
         }
 
         return data;
@@ -211,7 +287,8 @@ msr_write( const int cpu, uint32_t reg, uint64_t data)
     {
         if (pwrite(FD[cpu], &data, sizeof(data), reg) != sizeof(data))
         {
-            ERROR_PRINT("cpu %d reg %x",cpu, reg);
+            ERROR_PRINT("Cannot write MSR reg 0x%x with WRMSR instruction on CPU %d\n",
+                        reg, cpu);
         }
     }
     else
