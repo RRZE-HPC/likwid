2eb0126387618e92d95e20f1a339cfa9b3a531ad
Thomas.Roehl@googlemail.com <Thomas.Roehl@googlemail.com@38d4caea-f284-64ea-201f-c04d7a3ea24d>
2014-08-08 13:39:08 +0000

    TRUNK: Rename of architectural counter map to register map

--- trunk/src/includes/perfmon_haswell_counters.h
+++ trunk/src/includes/perfmon_haswell_counters.h
@@ -31,7 +31,7 @@
 #define NUM_COUNTERS_HASWELL 12
 #define NUM_COUNTERS_CORE_HASWELL 7
 
-static PerfmonCounterMap haswell_counter_map[NUM_COUNTERS_HASWELL] = {
+static RegisterMap haswell_counter_map[NUM_COUNTERS_HASWELL] = {
     /* Fixed Counters: instructions retired, cycles unhalted core */
     {"FIXC0", PMC0, FIXED, MSR_PERF_FIXED_CTR_CTRL, MSR_PERF_FIXED_CTR0, 0, 0},
     {"FIXC1", PMC1, FIXED, MSR_PERF_FIXED_CTR_CTRL, MSR_PERF_FIXED_CTR1, 0, 0},
--- trunk/src/includes/perfmon_pm_counters.h
+++ trunk/src/includes/perfmon_pm_counters.h
@@ -31,7 +31,7 @@
 #define NUM_COUNTERS_PM 2
 #define NUM_COUNTERS_CORE_PM 2
 
-static PerfmonCounterMap pm_counter_map[NUM_COUNTERS_PM] = {
+static RegisterMap pm_counter_map[NUM_COUNTERS_PM] = {
     {"PMC0",PMC0, PMC, MSR_PERFEVTSEL0, MSR_PMC0, 0, 0},
     {"PMC1",PMC1, PMC, MSR_PERFEVTSEL1, MSR_PMC1, 0, 0}
 };
