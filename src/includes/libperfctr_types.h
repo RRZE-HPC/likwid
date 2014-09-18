beafcd29f78ef0e1e9b3db26ae7d0ee1ba950160
Thomas.Roehl <TomTheBear@users.noreply.github.com>
2014-09-18 13:39:37 +0000

    TRUNK: Changed Marker API structures to hold double values, not uint64_t anymore

--- src/includes/libperfctr_types.h
+++ src/includes/libperfctr_types.h
@@ -38,15 +38,15 @@ typedef struct LikwidThreadResults{
     double time;
     TimerData startTime;
     uint32_t count;
-    uint64_t StartPMcounters[NUM_PMC];
-    uint64_t PMcounters[NUM_PMC];
+    double StartPMcounters[NUM_PMC];
+    double PMcounters[NUM_PMC];
 } LikwidThreadResults;
 
 typedef struct {
     bstring  tag;
     double*  time;
     uint32_t*  count;
-    uint64_t** counters;
+    double** counters;
 } LikwidResults;
 
 #endif /*LIBPERFCTR_H*/
