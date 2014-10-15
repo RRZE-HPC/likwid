02be54c6215896dc6290192f40982059fbc92d76
Thomas.Roehl@googlemail.com <Thomas.Roehl@googlemail.com@38d4caea-f284-64ea-201f-c04d7a3ea24d>
2014-10-15 12:42:57 +0000

    TRUNK: Updated RDPMC enable kernel module

--- trunk/kernel/enable_rdpmc.c
+++ trunk/kernel/enable_rdpmc.c
@@ -4,24 +4,24 @@
 #include <linux/module.h>   /* Needed by all modules */
 #include <linux/kernel.h>   /* Needed for KERN_INFO */
 
-#define MODULE_PARAM(type, name, value, desc)			\
-	type name = value;					\
-	module_param(name, type, 0664);				\
-	MODULE_PARM_DESC(name, desc)
+#define MODULE_PARAM(type, name, value, desc) \
+    type name = value; \
+    module_param(name, type, 0664); \
+    MODULE_PARM_DESC(name, desc)
 
 MODULE_PARAM(int, debug, 0, "Debug output");
 
 
-static void printc4(void) {
+static uint64_t printc4(void) {
     uint64_t output;
     // Read back CR4 to check the bit.
     __asm__("\t mov %%cr4,%0" : "=r"(output));
-    printk(KERN_INFO "%llu", output);
+    return output;
 }
 
 static void setc4b8(void * info) {
     // Set CR4, Bit 8 (9th bit from the right)  to enable
-	__asm__("push   %rax\n\t"
+    __asm__("push   %rax\n\t"
             "mov    %cr4,%rax;\n\t"
             "or     $(1 << 8),%rax;\n\t"
             "mov    %rax,%cr4;\n\t"
@@ -30,14 +30,14 @@ static void setc4b8(void * info) {
     );
 
     if (debug) {
-        printk(KERN_INFO "Processor %d, RDPMC_ENABLE_BIT=%d\n", smp_processor_id(), printc4());
+        printk(KERN_INFO "Processor %d, RDPMC_ENABLE_BIT=%llu\n", smp_processor_id(), printc4());
     }
 }
 
 static void clearc4b8(void * info) {
     printc4();
-	__asm__("push   %rax\n\t"
-        	"push   %rbx\n\t"
+    __asm__("push   %rax\n\t"
+            "push   %rbx\n\t"
             "mov    %cr4,%rax;\n\t"
             "mov  $(1 << 8), %rbx\n\t"
             "not  %rbx\n\t"
@@ -49,7 +49,7 @@ static void clearc4b8(void * info) {
     );
     
     if (debug) {
-        printk(KERN_INFO "Processor %d, RDPMC_ENABLE_BIT=%d\n", smp_processor_id(), printc4());
+        printk(KERN_INFO "Processor %d, RDPMC_ENABLE_BIT=%llu\n", smp_processor_id(), printc4());
     }
 }
 
