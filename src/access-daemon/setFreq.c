ebf2d983e7902d42a8efbc0e671974b02642f43d
Thomas.Roehl <TomTheBear@users.noreply.github.com>
2014-10-29 18:06:20 +0000

    v3.1: Enable setting of frequency scaling governor without setting frequency

--- src/access-daemon/setFreq.c
+++ src/access-daemon/setFreq.c
@@ -28,6 +28,7 @@ int main (int argn, char** argv)
     char* gov;
     char* gpath = malloc(100);
     char* fpath = malloc(100);
+    FILE* f;
 
     if (argn < 3 || argn > 4)
     {
@@ -43,24 +44,27 @@ int main (int argn, char** argv)
         exit(EXIT_FAILURE);
     }
     freq  = atoi(argv[2]);
-    if (freq <= 0)
+    if (freq < 0)
     {
         fprintf(stderr, "Frequency must be greater than 0.\n");
         exit(EXIT_FAILURE);
     }
+    snprintf(gpath, 60, "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor", cpuid);
+    snprintf(fpath, 60, "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_setspeed", cpuid);
 
     if (argn == 4)
     {
         gov = argv[3];
 
-        if ((strncmp(gov,"ondemand",12)) && (strncmp(gov,"performance",12))) {
+        if ((strncmp(gov,"ondemand",12)) && (strncmp(gov,"performance",12))) 
+        {
             fprintf(stderr, "Invalid governor %s!\n",gov);
             return (EXIT_FAILURE);
         }
-        snprintf(gpath, 60, "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor", cpuid);
 
-        FILE* f = fopen(gpath, "w");
-        if (f == NULL) {
+        f = fopen(gpath, "w");
+        if (f == NULL) 
+        {
             fprintf(stderr, "Unable to open path for writing\n");
             return (EXIT_FAILURE);
         }
@@ -68,20 +72,21 @@ int main (int argn, char** argv)
         fclose(f);
         return(EXIT_SUCCESS);
     }
-
-    snprintf(gpath, 60, "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor", cpuid);
-    snprintf(fpath, 60, "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_setspeed", cpuid);
-
-    FILE* f = fopen(gpath, "w");
-    if (f == NULL) {
-        fprintf(stderr, "Unable to open path for writing\n");
-        return (EXIT_FAILURE);
+    else
+    {
+        f = fopen(gpath, "w");
+        if (f == NULL) 
+        {
+            fprintf(stderr, "Unable to open path for writing\n");
+            return (EXIT_FAILURE);
+        }
+        fprintf(f,"userspace");
+        fclose(f);
     }
-    fprintf(f,"userspace");
-    fclose(f);
 
     f = fopen(fpath, "w");
-    if (f == NULL) {
+    if (f == NULL) 
+    {
         fprintf(stderr, "Unable to open path for writing\n");
         return (EXIT_FAILURE);
     }
