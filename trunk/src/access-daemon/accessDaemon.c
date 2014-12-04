5a363d503fab96567127c34bf5be6944fdc93135
Thomas.Roehl@googlemail.com <Thomas.Roehl@googlemail.com@38d4caea-f284-64ea-201f-c04d7a3ea24d>
2014-12-04 13:00:43 +0000

    Allow access to OFFCORE_RESPONSE_1 register for Intel P6 systems

--- trunk/src/access-daemon/accessDaemon.c
+++ trunk/src/access-daemon/accessDaemon.c
@@ -205,7 +205,8 @@ static int allowed_intel(uint32_t reg)
             (reg == 0x19C)  ||
             (reg == 0x1A2)  ||
             (reg == 0x1AD)  ||
-            (reg == 0x1A6))
+            (reg == 0x1A6)  ||
+            (reg == 0x1A7))
     {
         return 1;
     }
