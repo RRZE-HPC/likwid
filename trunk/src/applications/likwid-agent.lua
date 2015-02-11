8f6eae6d52a9cd1b83dafc2ab136113c5e4a8f85
Thomas.Roehl@googlemail.com <Thomas.Roehl@googlemail.com@38d4caea-f284-64ea-201f-c04d7a3ea24d>
2015-02-11 09:47:23 +0000

    Multiple group handling for likwid-agent and output funtions for logging and gmetric

--- trunk/src/applications/likwid-agent.lua
+++ trunk/src/applications/likwid-agent.lua
@@ -3,19 +3,188 @@
 package.path = package.path .. ';' .. string.gsub(debug.getinfo(1).source, "^@(.+/)[^/]+$", "%1") .. '/?.lua'
 local likwid = require("likwid")
 
+dconfig = {}
+dconfig["groupStrings"] ={}
+dconfig["groupData"] ={}
+dconfig["duration"] = 1
+dconfig["groupPath"] = "/home/rrze/unrz/unrz139/TMP/likwid-base/trunk/gmon_module/groups"
+dconfig["logPath"] = nil
+dconfig["gmetricPath"] = "gmetric"
+dconfig["gmetricConfig"] = nil
+dconfig["rrd"] = false
+
+
+local function read_daemon_config(filename)
+    if filename == nil or filename == "" then
+        print("Not a valid config filename")
+        os.exit(1)
+    end
+    local f = io.open(filename, "r")
+    if f == nil then
+        print("Cannot open config file "..filename)
+        os.exit(1)
+    end
+    local t = f:read("*all")
+    f:close()
+
+    for i, line in pairs(likwid.stringsplit(t,"\n")) do
+        local s,e = line:find("GROUPPATH")
+        if s ~= nil then
+            local linelist = stringsplit(line, "%s+", nil, "%s+")
+            table.remove(linelist, 1)
+            dconfig["groupPath"] = linelist[1]
+        end
+        local s,e = line:find("EVENTSET")
+        if s ~= nil then
+            local linelist = stringsplit(line, "%s+", nil, "%s+")
+            table.remove(linelist, 1)
+            table.insert(dconfig["groupStrings"], table.concat(linelist, " "))
+        end
+        local s,e = line:find("DURATION")
+        if s ~= nil then
+            local linelist = stringsplit(line, "%s+", nil, "%s+")
+            table.remove(linelist, 1)
+            dconfig["duration"] = tonumber(linelist[1])
+        end
+        local s,e = line:find("LOGPATH")
+        if s ~= nil then
+            local linelist = stringsplit(line, "%s+", nil, "%s+")
+            table.remove(linelist, 1)
+            dconfig["logPath"] = linelist[1]
+        end
+        local s,e = line:find("GMETRICPATH")
+        if s ~= nil then
+            local linelist = stringsplit(line, "%s+", nil, "%s+")
+            table.remove(linelist, 1)
+            dconfig["gmetricPath"] = linelist[1]
+        end
+        local s,e = line:find("GMETRICCONFIG")
+        if s ~= nil then
+            local linelist = stringsplit(line, "%s+", nil, "%s+")
+            table.remove(linelist, 1)
+            dconfig["gmetricConfig"] = linelist[1]
+        end
+        local s,e = line:find("RRD")
+        if s ~= nil then
+            local linelist = stringsplit(line, "%s+", nil, "%s+")
+            table.remove(linelist, 1)
+            if linelist[1] == "True" then
+                dconfig["rrd"] = true
+            end
+        end
+    end
+end
+
+local function calc_avg(key, results)
+    local sum = 0
+    local numThreads = likwid.getNumberOfThreads()
+    for thread=1, numThreads do
+        sum = sum + results[thread][key]
+    end
+    return sum/numThreads
+end
+
+local function calc_sum(key, results)
+    local sum = 0
+    local numThreads = likwid.getNumberOfThreads()
+    for thread=1, numThreads do
+        sum = sum + results[thread][key]
+    end
+    return sum
+end
+
+local function calc_min(key, results)
+    local min = math.huge
+    local numThreads = likwid.getNumberOfThreads()
+    for thread=1, numThreads do
+        if results[thread][key] < min then
+            min = results[thread][key]
+        end
+    end
+    return min
+end
+
+local function calc_max(key, results)
+    local max = 0
+    local numThreads = likwid.getNumberOfThreads()
+    for thread=1, numThreads do
+        if results[thread][key] > max then
+            max = results[thread][key]
+        end
+    end
+    return max
+end
+
+local function logger(results)
+    local f = io.open(dconfig["logPath"], "a")
+    if f == nil then
+        print("Cannot open logfile ".. dconfig["logPath"])
+        return
+    end
+    local timestamp = results["Timestamp"]
+    for k,v in pairs(results) do
+        if k ~= "Timestamp" then
+            f:write(timestamp..","..k.. ","..v.."\n")
+        end
+    end
+    f:close()
+end
+
+local function gmetric(gdata, results)
+    execList = {}
+    local f = io.popen("hostname -f","r")
+    local hostname = f:read("*all"):gsub("^%s*(.-)%s*$", "%1")
+    f:close()
+    table.insert(execList, dconfig["gmetricPath"])
+    table.insert(execList, "-t")
+    table.insert(execList, "double")
+    table.insert(execList, "-S")
+    
+    table.insert(execList, hostname)
+    if dconfig["gmetricConfig"] ~= nil then
+        table.insert(execList, "-c")
+        table.insert(execList, dconfig["gmetricConfig"])
+    end
+    if gdata["GroupString"] ~= gdata["EventString"] then
+        table.insert(execList, "-g")
+        table.insert(execList, gdata["GroupString"])
+    end
+    for k,v in pairs(results) do
+        local execStr = table.concat(execList, " ")
+        local name = k
+        local unit = nil
+        local s,e = k:find("%[")
+        if s ~= nil then
+            name = k:sub(0,s-1):gsub("^%s*(.-)%s*$", "%1")
+            unit = k:sub(s+1,k:len()-1):gsub("^%s*(.-)%s*$", "%1")
+        end
+        execStr = execStr .. " --name=\"" .. name .."\""
+        if unit ~= nil then
+            execStr = execStr .. " --units=\"" .. unit .."\""
+        end
+        execStr = execStr .. " --value=\"" .. tostring(v) .."\""
+        print(execStr)
+    end
+end
+
+local function rrd(results)
+    print("RRD not implemented")
+end
+
 -- Read commandline arguments
-if #arg ~= 2 then
-    print("Need EventSet and measurement duration in seconds on commandline")
+if #arg ~= 1 then
+    print("Usage:")
+    print(arg[0] .. " <configFile>")
     os.exit(1)
 end
-local eventSet = arg[1]
-local duration = arg[2]
 
 -- Get architectural information for the current system
 local cpuinfo = likwid.getCpuInfo()
 local cputopo = likwid.getCpuTopology()
--- Read configuration file
+-- Read LIKWID configuration file, mainly to avoid topology lookup
 local config = likwid.getConfiguration()
+-- Read LIKWID daemon configuration file
+read_daemon_config(arg[1])
 
 -- Add all cpus to the cpulist
 local cpulist = {}
@@ -32,38 +201,109 @@ if likwid.setAccessClientMode(access_mode) ~= 0 then
     os.exit(1)
 end
 
+-- Select group directory for monitoring
+likwid.groupfolder = dconfig["groupPath"]
+
 -- Evaluate eventSet given on commandline. If it's a group, resolve to events
-local s,e = eventSet:find(":")
-gdata = nil
-if s == nil then
-    gdata = likwid.get_groupdata(cpuinfo["short_name"], eventSet)
-    eventSet = gdata["EventString"]
-else
-    gdata = likwid.new_groupdata(eventSet)
+for k,v in pairs(dconfig["groupStrings"]) do
+    local s,e = v:find(":")
+    local gdata = nil
+    if s == nil then
+        gdata = likwid.get_groupdata(cpuinfo["short_name"], v)
+    else
+        gdata = likwid.new_groupdata(v)
+    end
+    table.insert(dconfig["groupData"], gdata)
+end
+if #dconfig["groupData"] == 0 then
+    print("None of the event strings can be added for current architecture.")
+    os.exit(1)
 end
 
 -- Initialize likwid perfctr
 likwid.init(cputopo["numHWThreads"], cpulist)
-local groupID = likwid.addEventSet(eventSet)
+for k,v in pairs(dconfig["groupData"]) do
+    local groupID = likwid.addEventSet(v["EventString"])
+end
 
-likwid.setupCounters(groupID)
+while true do
 
--- Perform the measurement
-likwid.startCounters()
-sleep(duration)
-likwid.stopCounters()
+    for groupID,gdata in pairs(dconfig["groupData"]) do
+        local old_mtime = likwid_getRuntimeOfGroup(groupID)* 1.E-06
+        local cur_time = os.time()
+        likwid.setupCounters(groupID)
 
--- Read all results and print them in CSV
-local results = likwid.getResults()
-for ig,g in pairs(results) do
-    str = tostring(ig)..","..tostring(cputopo["numHWThreads"])
-    for ie=0,#g do
-        str = str .. "," ..gdata["Events"][ie]["Event"]
-        for it=0,#g[ie] do
-            str = str .. ","..tostring(g[ie][it])
+        -- Perform the measurement
+        likwid.startCounters()
+        sleep(dconfig["duration"])
+        likwid.stopCounters()
+
+        -- temporal array collecting counter to values for each thread for metric calculation
+        threadResults = {}
+        local mtime = likwid_getRuntimeOfGroup(groupID)* 1.E-06
+        local clock = likwid_getCpuClock();
+
+        for event=1, likwid.getNumberOfEvents(groupID) do
+            for thread=1, likwid.getNumberOfThreads() do
+                if threadResults[thread] == nil then
+                    threadResults[thread] = {}
+                end
+                --print(thread, old_mtime, mtime, mtime - old_mtime)
+                threadResults[thread]["time"] = mtime - old_mtime
+                threadResults[thread]["inverseClock"] = 1.0/clock;
+                local result = likwid.getResult(groupID, event-1, thread-1)
+                if threadResults[thread][gdata["Events"][event-1]["Counter"]] == nil then
+                    threadResults[thread][gdata["Events"][event-1]["Counter"]] = result
+                end
+            end
+        end
+
+
+        if gdata["Metrics"] then
+            local threadOutput = {}
+            for i, metric in pairs(gdata["Metrics"]) do
+                for thread=1, likwid.getNumberOfThreads() do
+                    if threadOutput[thread] == nil then
+                        threadOutput[thread] = {}
+                    end
+                    local result = likwid.calculate_metric(metric["formula"], threadResults[thread])
+                    threadOutput[thread][metric["description"]] = result
+                end
+            end
+            output = {}
+            output["Timestamp"] = os.date("%m/%d/%Y_%X",cur_time)
+            for i, metric in pairs(gdata["Metrics"]) do
+                itemlist = stringsplit(metric["description"], "%s+", nil, "%s+")
+                func = itemlist[1]
+                table.remove(itemlist, 1)
+                desc = table.concat(itemlist," ")
+                if func == "AVG" then
+                    output[metric["description"]] = calc_avg(metric["description"], threadOutput)
+                elseif func == "SUM" then
+                    output[metric["description"]] = calc_sum(metric["description"], threadOutput)
+                elseif func == "MIN" then
+                    output[metric["description"]] = calc_sum(metric["description"], threadOutput)
+                elseif func == "MAX" then
+                    output[metric["description"]] = calc_sum(metric["description"], threadOutput)
+                elseif func == "ONCE" then
+                    output[desc] = threadOutput[1][metric["description"]]
+                else
+                    for thread=1, likwid.getNumberOfThreads() do
+                        output["T"..cpulist[thread] .. " " .. desc] = threadOutput[thread][metric["description"]]
+                    end
+                end
+            end
+            if dconfig["logPath"] ~= nil then
+                logger(output)
+            end
+            if dconfig["gmetricPath"] ~= nil then
+                gmetric(gdata, output)
+            end
+            if dconfig["rrd"] then
+                rrd(output)
+            end
         end
     end
-    print(str)
 end
 
 -- Finalize likwid perfctr
