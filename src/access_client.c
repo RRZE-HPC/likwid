RRZE-HPC/likwid

commit abd16cce0098dc25c321f45e272691976abfc6ee

Some changes to the build system. Path for sockets between library and access daemon is now configurable. Thanks TZ for the patches.

src/access-daemon/Makefile
@@ -33,7 +33,7 @@ DAEMON_TARGET = likwid-accessD
 SETFREQ_TARGET = likwid-setFreq
 Q         ?= @
 
-DEFINES   += -D_GNU_SOURCE -DMAX_NUM_THREADS=$(MAX_NUM_THREADS) -DMAX_NUM_NODES=$(MAX_NUM_NODES)
+DEFINES   += -D_GNU_SOURCE -DMAX_NUM_THREADS=$(MAX_NUM_THREADS) -DMAX_NUM_NODES=$(MAX_NUM_NODES) -DLIKWIDLOCK=$(LIKWIDLOCKPATH)
 INCLUDES  = -I../includes
 CFLAGS    += -std=c99 -fPIC -pie -fPIE -fstack-protector
 ifeq ($(COMPILER),GCCX86)

src/access_client.c
@@ -159,7 +159,7 @@ access_client_startDaemon(int cpu_id)
 
     address.sun_family = AF_LOCAL;
     address_length = sizeof(address);
-    snprintf(address.sun_path, sizeof(address.sun_path), "/tmp/likwid-%d", pid);
+    snprintf(address.sun_path, sizeof(address.sun_path), TOSTRING(LIKWIDSOCKETBASE) "-%d", pid);
     filepath = strdup(address.sun_path);
 
     while (timeout > 0)

Makefile
@@ -32,6 +32,7 @@ GROUP_DIR   = ./groups
 FILTER_DIR  = ./filters
 MAKE_DIR    = ./make
 
+Q         ?= @
 
 #DO NOT EDIT BELOW
 
@@ -135,18 +136,18 @@ $(DYNAMIC_TARGET_LIB): $(BUILD_DIR) $(PERFMONHEADERS) $(OBJ) $(TARGET_HWLOC_LIB)
 
 $(DAEMON_TARGET): $(SRC_DIR)/access-daemon/accessDaemon.c
 	@echo "===>  BUILD access daemon likwid-accessD"
-	$(Q)$(MAKE) -s -C  $(SRC_DIR)/access-daemon likwid-accessD
+	$(Q)$(MAKE) -C  $(SRC_DIR)/access-daemon likwid-accessD
 
 $(FREQ_TARGET): $(SRC_DIR)/access-daemon/setFreq.c
 	@echo "===>  BUILD frequency daemon likwid-setFreq"
-	$(Q)$(MAKE) -s -C  $(SRC_DIR)/access-daemon likwid-setFreq
+	$(Q)$(MAKE) -C  $(SRC_DIR)/access-daemon likwid-setFreq
 
 $(BUILD_DIR):
 	@mkdir $(BUILD_DIR)
 
 $(PINLIB):
 	@echo "===>  CREATE LIB  $(PINLIB)"
-	$(Q)$(MAKE) -s -C src/pthread-overload/ $(PINLIB)
+	$(Q)$(MAKE) -C src/pthread-overload/ $(PINLIB)
 
 $(GENGROUPLOCK): $(foreach directory,$(shell ls $(GROUP_DIR)), $(wildcard $(GROUP_DIR)/$(directory)/*.txt))
 	@echo "===>  GENERATE GROUP HEADERS"
@@ -160,15 +161,15 @@ $(FORTRAN_IF): $(SRC_DIR)/likwid.f90
 
 $(TARGET_LUA_LIB):
 	@echo "===>  ENTER  $(LUA_FOLDER)"
-	$(Q)$(MAKE) -s --no-print-directory -C $(LUA_FOLDER) $(MAKECMDGOALS)
+	$(Q)$(MAKE) --no-print-directory -C $(LUA_FOLDER) $(MAKECMDGOALS)
 
 $(TARGET_HWLOC_LIB):
 	@echo "===>  ENTER  $(HWLOC_FOLDER)"
-	$(Q)$(MAKE) -s --no-print-directory -C $(HWLOC_FOLDER) $(MAKECMDGOALS)
+	$(Q)$(MAKE) --no-print-directory -C $(HWLOC_FOLDER) $(MAKECMDGOALS)
 
 $(BENCH_TARGET):
 	@echo "===>  ENTER  $(BENCH_FOLDER)"
-	$(Q)$(MAKE) -s --no-print-directory -C $(BENCH_FOLDER) $(MAKECMDGOALS)
+	$(Q)$(MAKE) --no-print-directory -C $(BENCH_FOLDER) $(MAKECMDGOALS)
 
 #PATTERN RULES
 $(BUILD_DIR)/%.o:  %.c

src/pthread-overload/Makefile
@@ -33,6 +33,7 @@ include  ../../make/include_$(COMPILER).mk
 include  ../../make/config_checks.mk
 include  ../../make/config_defines.mk
 
+Q         ?= @
 
 TARGET   = $(PINLIB)
 
@@ -48,5 +49,5 @@ CPPFLAGS := $(CPPFLAGS) $(DEFINES) $(INCLUDES)
 all: $(TARGET)
 
 $(TARGET): pthread-overload.c
-	$(CC) -Wl,-soname,$(TARGET).$(VERSION).$(RELEASE) $(CFLAGS) $(ANSI_CFLAGS) $(CPPFLAGS) $(INCLUDES) $(SHARED_CFLAGS) $(SHARED_LFLAGS) -o ../../$(TARGET) pthread-overload.c $(LIBS)
+	$(Q)$(CC) -Wl,-soname,$(TARGET).$(VERSION).$(RELEASE) $(CFLAGS) $(ANSI_CFLAGS) $(CPPFLAGS) $(INCLUDES) $(SHARED_CFLAGS) $(SHARED_LFLAGS) -o ../../$(TARGET) pthread-overload.c $(LIBS)
 

config.mk
@@ -81,6 +81,7 @@ DATE    = 19.05.2016
 
 RPATHS = -Wl,-rpath=$(INSTALLED_LIBPREFIX)
 LIKWIDLOCKPATH = /var/run/likwid.lock
+LIKWIDSOCKETBASE = /tmp/likwid  # -%d will be added automatically to the socket name
 LIBLIKWIDPIN = $(abspath $(INSTALLED_PREFIX)/lib/liblikwidpin.so.$(VERSION).$(RELEASE))
 LIKWIDFILTERPATH = $(abspath $(INSTALLED_PREFIX)/share/likwid/filter)
 LIKWIDGROUPPATH = $(abspath $(INSTALLED_PREFIX)/share/likwid/perfgroups)

make/config_defines.mk
@@ -8,6 +8,7 @@ DEFINES   += -DVERSION=$(VERSION)         \
 		 -DACCESSDAEMON=$(INSTALLED_ACCESSDAEMON) \
 		 -DGROUPPATH=$(LIKWIDGROUPPATH) \
 		 -DLIKWIDLOCK=$(LIKWIDLOCKPATH) \
+		 -DLIKWIDSOCKETBASE=$(LIKWIDSOCKETBASE) \
 		 -D_GNU_SOURCE
 
 DYNAMIC_TARGET_LIB := liblikwid.so

src/access-daemon/accessDaemon.c
@@ -1142,7 +1142,7 @@ int main(void)
 
     /* setup filename for socket */
     filepath = (char*) calloc(sizeof(addr1.sun_path), 1);
-    snprintf(filepath, sizeof(addr1.sun_path), "/tmp/likwid-%d", pid);
+    snprintf(filepath, sizeof(addr1.sun_path), TOSTRING(LIKWIDSOCKETBASE) "-%d", pid);
 
     /* get a socket */
     LOG_AND_EXIT_IF_ERROR(sockfd = socket(AF_LOCAL, SOCK_STREAM, 0), socket failed);

commit 885bd33dcbefdad610a941ffeb0da9f6287e4e91

Fixing bug introduced with commit https://github.com/RRZE-HPC/likwid/commit/abd16cce0098dc25c321f45e272691976abfc6ee

src/access-daemon/Makefile
@@ -33,7 +33,7 @@ DAEMON_TARGET = likwid-accessD
 SETFREQ_TARGET = likwid-setFreq
 Q         ?= @
 
-DEFINES   += -D_GNU_SOURCE -DMAX_NUM_THREADS=$(MAX_NUM_THREADS) -DMAX_NUM_NODES=$(MAX_NUM_NODES) -DLIKWIDLOCK=$(LIKWIDLOCKPATH)
+DEFINES   += -D_GNU_SOURCE -DMAX_NUM_THREADS=$(MAX_NUM_THREADS) -DMAX_NUM_NODES=$(MAX_NUM_NODES) -DLIKWIDLOCK=$(LIKWIDLOCKPATH) -DLIKWIDSOCKETBASE=$(LIKWIDSOCKETBASE)
 INCLUDES  = -I../includes
 CFLAGS    += -std=c99 -fPIC -pie -fPIE -fstack-protector
 ifeq ($(COMPILER),GCCX86)

src/access_client.c
@@ -180,11 +180,10 @@ access_client_startDaemon(int cpu_id)
     if (timeout <= 0)
     {
         ERRNO_PRINT;  /* should hopefully still work, as we make no syscalls in between. */
-        fprintf(stderr, "Exiting due to timeout: The socket file at '%s' \
-                could not be opened within 10 seconds.\n", filepath);
-        fprintf(stderr, "Consult the error message above this to find out why.\n");
-        fprintf(stderr, "If the error is 'no such file or directoy', \
-                it usually means that likwid-accessD just failed to start.\n");
+        fprintf(stderr, "Exiting due to timeout: The socket file at '%s' could not be\n", filepath);
+        fprintf(stderr, "opened within 10 seconds. Consult the error message above\n");
+        fprintf(stderr, "this to find out why. If the error is 'no such file or directoy',\n");
+        fprintf(stderr, "it usually means that likwid-accessD just failed to start.\n");
         exit(EXIT_FAILURE);
     }
     DEBUG_PRINT(DEBUGLEV_INFO, Successfully opened socket %s to daemon for CPU %d, filepath, cpu_id);