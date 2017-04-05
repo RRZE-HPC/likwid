RRZE-HPC/likwid

commit b02ee3e9b705dc68ffe9be6442c7e33e51510b1e

Fix for the configuration module to work when ACCESSMODE=accessdaemon but BUILDDAEMON=false. Only likwid-topology and likwid-pin use this feature, the other ones commonly require a working access module

src/applications/likwid-pin.lua
@@ -89,6 +89,14 @@ local function usage()
     examples()
 end
 
+local function close_and_exit(code)
+    likwid.putTopology()
+    likwid.putAffinityInfo()
+    likwid.putConfiguration()
+    likwid.unsetenv("LIKWID_NO_ACCESS")
+    os.exit(code)
+end
+
 delimiter = ','
 quiet = 0
 sweep_sockets = false
@@ -99,7 +107,7 @@ skip_mask = nil
 affinity = nil
 num_threads = 0
 
-
+likwid.setenv("LIKWID_NO_ACCESS", "1")
 config = likwid.getConfiguration()
 cputopo = likwid.getCpuTopology()
 affinity = likwid.getAffinityInfo()
@@ -112,16 +120,10 @@ end
 for opt,arg in likwid.getopt(arg, {"c:", "d:", "h", "i", "p", "q", "s:", "S", "t:", "v", "V:", "verbose:", "help", "version", "skip","sweep", "quiet"}) do
     if opt == "h" or opt == "help" then
         usage()
-        likwid.putTopology()
-        likwid.putAffinityInfo()
-        likwid.putConfiguration()
-        os.exit(0)
+        close_and_exit(0)
     elseif opt == "v" or opt == "version" then
         version()
-        likwid.putTopology()
-        likwid.putAffinityInfo()
-        likwid.putConfiguration()
-        os.exit(0)
+        close_and_exit(0)
     elseif opt == "V" or opt == "verbose" then
         verbose = tonumber(arg)
         likwid.setVerbosity(verbose)
@@ -133,20 +135,14 @@ for opt,arg in likwid.getopt(arg, {"c:", "d:", "h", "i", "p", "q", "s:", "S", "t
         end
         if (num_threads == 0) then
             print_stderr("Failed to parse cpulist " .. arg)
-            likwid.putTopology()
-            likwid.putAffinityInfo()
-            likwid.putConfiguration()
-            os.exit(1)
+            close_and_exit(1)
         end
     elseif (opt == "d") then
         delimiter = arg
     elseif opt == "S" or opt == "sweep" then
         if (affinity == nil) then
             print_stderr("Option -S is not supported for unknown processor!")
-            likwid.putTopology()
-            likwid.putAffinityInfo()
-            likwid.putConfiguration()
-            os.exit(1)
+            close_and_exit(1)
         end
         sweep_sockets = true
     elseif (opt == "i") then
@@ -157,7 +153,7 @@ for opt,arg in likwid.getopt(arg, {"c:", "d:", "h", "i", "p", "q", "s:", "S", "t
         local s,e = arg:find("0x")
         if s == nil then
             print_stderr("Skip mask must be given in hex, hence start with 0x")
-            os.exit(1)
+            close_and_exit(1)
         end
         skip_mask = arg
     elseif opt == "q" or opt == "quiet" then
@@ -165,16 +161,10 @@ for opt,arg in likwid.getopt(arg, {"c:", "d:", "h", "i", "p", "q", "s:", "S", "t
         quiet = 1
     elseif opt == "?" then
         print_stderr("Invalid commandline option -"..arg)
-        likwid.putTopology()
-        likwid.putAffinityInfo()
-        likwid.putConfiguration()
-        os.exit(1)
+        close_and_exit(1)
     elseif opt == "!" then
         print_stderr("Option requires an argument")
-        likwid.putTopology()
-        likwid.putAffinityInfo()
-        likwid.putConfiguration()
-        os.exit(1)
+        close_and_exit(1)
     end
 end
 
@@ -185,20 +175,14 @@ if print_domains and num_threads > 0 then
         outstr = outstr .. delimiter .. cpu
     end
     print_stdout(outstr:sub(2,outstr:len()))
-    likwid.putTopology()
-    likwid.putAffinityInfo()
-    likwid.putConfiguration()
-    os.exit(0)
+    close_and_exit(1)
 elseif print_domains then
     for k,v in pairs(affinity["domains"]) do
         print_stdout(string.format("Domain %s:", v["tag"]))
         print_stdout("\t" .. table.concat(v["processorList"], ","))
         print_stdout("")
     end
-    likwid.putTopology()
-    likwid.putAffinityInfo()
-    likwid.putConfiguration()
-    os.exit(0)
+    close_and_exit(1)
 end
 
 if num_threads == 0 then
@@ -206,7 +190,7 @@ if num_threads == 0 then
 end
 if (#arg == 0) then
     print_stderr("Executable must be given on commandline")
-    os.exit(1)
+    close_and_exit(1)
 end
 
 if interleaved_policy then
@@ -269,10 +253,7 @@ local exec = table.concat(arg," ",1, likwid.tablelength(arg)-2)
 local pid = likwid.startProgram(exec, num_threads, cpu_list)
 if (pid == nil) then
     print_stderr("Failed to execute command: ".. exec)
-    likwid.putTopology()
-    likwid.putAffinityInfo()
-    likwid.putConfiguration()
-    os.exit(1)
+    close_and_exit(1)
 end
 
 local exitvalue = likwid.waitpid(pid)
@@ -280,4 +261,4 @@ local exitvalue = likwid.waitpid(pid)
 likwid.putAffinityInfo()
 likwid.putTopology()
 likwid.putConfiguration()
-os.exit(exitvalue)
+close_and_exit(exitvalue)

src/applications/likwid-topology.lua
@@ -109,6 +109,7 @@ for opt,arg in likwid.getopt(arg, {"h","v","c","C","g","o:","V:","O","help","ver
     end
 end
 
+likwid.setenv("LIKWID_NO_ACCESS", "1")
 local config = likwid.getConfiguration()
 local cpuinfo = likwid.getCpuInfo()
 local cputopo = likwid.getCpuTopology()
@@ -398,4 +399,5 @@ likwid.putAffinityInfo()
 likwid.putNumaInfo()
 likwid.putTopology()
 likwid.putConfiguration()
+likwid.unsetenv("LIKWID_NO_ACCESS")
 os.exit(0)

src/applications/likwid.lua
@@ -108,6 +108,7 @@ likwid.memSweep = likwid_memSweep
 likwid.memSweepDomain = likwid_memSweepDomain
 likwid.pinProcess = likwid_pinProcess
 likwid.setenv = likwid_setenv
+likwid.unsetenv = likwid_unsetenv
 likwid.getpid = likwid_getpid
 likwid.setVerbosity = likwid_setVerbosity
 likwid.access = likwid_access

src/luawid.c
@@ -120,29 +120,39 @@ lua_likwid_getConfiguration(lua_State* L)
     {
         configfile = get_configuration();
     }
-    lua_newtable(L);
-    lua_pushstring(L, "configFile");
-    lua_pushstring(L, configfile->configFileName);
-    lua_settable(L,-3);
-    lua_pushstring(L, "topologyFile");
-    lua_pushstring(L, configfile->topologyCfgFileName);
-    lua_settable(L,-3);
-    lua_pushstring(L, "daemonPath");
-    lua_pushstring(L, configfile->daemonPath);
-    lua_settable(L,-3);
-    lua_pushstring(L, "groupPath");
-    lua_pushstring(L, configfile->groupPath);
-    lua_settable(L,-3);
-    lua_pushstring(L, "daemonMode");
-    lua_pushinteger(L, (int)configfile->daemonMode);
-    lua_settable(L,-3);
-    lua_pushstring(L, "maxNumThreads");
-    lua_pushinteger(L, configfile->maxNumThreads);
-    lua_settable(L,-3);
-    lua_pushstring(L, "maxNumNodes");
-    lua_pushinteger(L, configfile->maxNumNodes);
-    lua_settable(L,-3);
-    return 1;
+    if (configfile)
+    {
+        lua_newtable(L);
+        lua_pushstring(L, "configFile");
+        if (configfile->configFileName != NULL)
+            lua_pushstring(L, configfile->configFileName);
+        else
+            lua_pushnil(L);
+        lua_settable(L,-3);
+        lua_pushstring(L, "topologyFile");
+        lua_pushstring(L, configfile->topologyCfgFileName);
+        lua_settable(L,-3);
+        lua_pushstring(L, "daemonPath");
+        if (configfile->daemonPath != NULL)
+            lua_pushstring(L, configfile->daemonPath);
+        else
+            lua_pushnil(L);
+        lua_settable(L,-3);
+        lua_pushstring(L, "groupPath");
+        lua_pushstring(L, configfile->groupPath);
+        lua_settable(L,-3);
+        lua_pushstring(L, "daemonMode");
+        lua_pushinteger(L, (int)configfile->daemonMode);
+        lua_settable(L,-3);
+        lua_pushstring(L, "maxNumThreads");
+        lua_pushinteger(L, configfile->maxNumThreads);
+        lua_settable(L,-3);
+        lua_pushstring(L, "maxNumNodes");
+        lua_pushinteger(L, configfile->maxNumNodes);
+        lua_settable(L,-3);
+        return 1;
+    }
+    return 0;
 }
 
 static int
@@ -2047,6 +2057,14 @@ lua_likwid_setenv(lua_State* L)
 }
 
 static int
+lua_likwid_unsetenv(lua_State* L)
+{
+    const char* element = (const char*)luaL_checkstring(L, -1);
+    unsetenv(element);
+    return 0;
+}
+
+static int
 lua_likwid_getpid(lua_State* L)
 {
     lua_pushinteger(L, (lua_Integer)(getpid()));
@@ -2715,6 +2733,7 @@ luaopen_liblikwid(lua_State* L){
     lua_register(L, "likwid_pinProcess", lua_likwid_pinProcess);
     // Helper functions
     lua_register(L, "likwid_setenv", lua_likwid_setenv);
+    lua_register(L, "likwid_unsetenv", lua_likwid_unsetenv);
     lua_register(L, "likwid_getpid", lua_likwid_getpid);
     lua_register(L, "likwid_access", lua_likwid_access);
     lua_register(L, "likwid_startProgram", lua_likwid_startProgram);

commit 088480f8205d1d76aa2a8beeab8085bbfcb967b1

Copy-paste-error in likwid-pin introduced in commit https://github.com/RRZE-HPC/likwid/commit/b02ee3e9b705dc68ffe9be6442c7e33e51510b1e

src/applications/likwid-pin.lua
@@ -175,7 +175,7 @@ if print_domains and num_threads > 0 then
         outstr = outstr .. delimiter .. cpu
     end
     print_stdout(outstr:sub(2,outstr:len()))
-    close_and_exit(1)
+    close_and_exit(0)
 elseif print_domains then
     for k,v in pairs(affinity["domains"]) do
         print_stdout(string.format("Domain %s:", v["tag"]))