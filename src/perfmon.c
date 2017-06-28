RRZE-HPC/likwid

commit d92550697841d93f47a608efa335f69dc6b567ae

Reduce overhead at result calculation

src/perfmon.c
@@ -590,6 +590,7 @@ calculateResult(int groupId, int eventId, int threadId)
     PerfmonCounter* counter;
     int cpu_id;
     double result = 0.0;
+    uint64_t maxValue = 0ULL;
     if (groupSet->groups[groupId].events[eventId].type == NOTYPE)
         return result;
 
@@ -601,11 +602,13 @@ calculateResult(int groupId, int eventId, int threadId)
     }
     else if (counter->overflows > 0)
     {
-        result += (double) ((perfmon_getMaxCounterValue(counter_map[event->index].type) -
-                    counter->startData) + counter->counterData);
-        counter->overflows--;
+        maxValue = perfmon_getMaxCounterValue(counter_map[event->index].type);
+        result += (double) ((maxValue - counter->startData) + counter->counterData);
+        if (counter->overflows > 1)
+        {
+            result += (double) ((counter->overflows-1) * maxValue);
+        }
     }
-    result += (double) (counter->overflows * perfmon_getMaxCounterValue(counter_map[event->index].type));
     if (counter_map[event->index].type == POWER)
     {
         result *= power_getEnergyUnit(getCounterTypeOffset(event->index));

commit 1501bddfc593f72c480ef7d916fbd2d1764a3b4f

No need anymore to reset overflow count at read. Caused by commit d92550697841d93f47a608efa335f69dc6b567ae

src/perfmon.c
@@ -2022,7 +2022,6 @@ __perfmon_readCounters(int groupId, int threadId)
                     groupSet->groups[groupId].events[j].threadCounter[threadId].fullResult += result;
                     groupSet->groups[groupId].events[j].threadCounter[threadId].startData =
                         groupSet->groups[groupId].events[j].threadCounter[threadId].counterData;
-                    groupSet->groups[groupId].events[j].threadCounter[threadId].overflows = 0;
                 }
             }
         }
@@ -2041,7 +2040,6 @@ __perfmon_readCounters(int groupId, int threadId)
             groupSet->groups[groupId].events[j].threadCounter[threadId].fullResult += result;
             groupSet->groups[groupId].events[j].threadCounter[threadId].startData =
                 groupSet->groups[groupId].events[j].threadCounter[threadId].counterData;
-            groupSet->groups[groupId].events[j].threadCounter[threadId].overflows = 0;
         }
 }
     timer_start(&groupSet->groups[groupId].timer);
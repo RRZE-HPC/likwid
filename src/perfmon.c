1501bddfc593f72c480ef7d916fbd2d1764a3b4f
TomTheBear <thomas.roehl@fau.de>
2017-06-28 15:57:10 +0200

    No need anymore to reset overflow count at read. Caused by commit d92550697841d93f47a608efa335f69dc6b567ae

--- src/perfmon.c
+++ src/perfmon.c
@@ -2022,7 +2022,6 @@ __perfmon_readCounters(int groupId, int threadId)
                     groupSet->groups[groupId].events[j].threadCounter[threadId].fullResult += result;
                     groupSet->groups[groupId].events[j].threadCounter[threadId].startData =
                         groupSet->groups[groupId].events[j].threadCounter[threadId].counterData;
-                    groupSet->groups[groupId].events[j].threadCounter[threadId].overflows = 0;
                 }
             }
         }
@@ -2041,7 +2040,6 @@ __perfmon_readCounters(int groupId, int threadId)
             groupSet->groups[groupId].events[j].threadCounter[threadId].fullResult += result;
             groupSet->groups[groupId].events[j].threadCounter[threadId].startData =
                 groupSet->groups[groupId].events[j].threadCounter[threadId].counterData;
-            groupSet->groups[groupId].events[j].threadCounter[threadId].overflows = 0;
         }
 }
     timer_start(&groupSet->groups[groupId].timer);
