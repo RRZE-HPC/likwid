bf934d90353062893593886a4fbc2f81b4053fff
TomTheBear <thomas.roehl@fau.de>
2017-10-26 15:35:13 +0200

    Fix for internal calculator

--- src/calculator.c
+++ src/calculator.c
@@ -1025,7 +1025,7 @@ bool postfix(token *tokens, int numTokens, Stack *output)
     // free remaining intermediate results
     while (stackSize(&intermediate) > 0)
     {
-        stackPop(&intermediate);
+        free(stackPop(&intermediate));
     }
     if (err == true)
     {
@@ -1067,6 +1067,7 @@ calculate_infix(char* finfix, double *result)
                 tokens[i] = NULL;
         }
         *result = strtod((char*)stackTop(&expr), NULL);
+        free(stackPop(&expr));
     }
     ret = 0;
 calcerror:
