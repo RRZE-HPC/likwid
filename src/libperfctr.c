e57925ebda2c608b694af31735d722b177af8304
Thomas Roehl <thomas.roehl@fau.de>
2018-04-17 13:04:32 +0200

    Perform one register read in LIKWID_MARKER_REGISTER to avoid establishing the connection with the access daemon in LIKWID_MARKER_START

--- src/libperfctr.c
+++ src/libperfctr.c
@@ -498,6 +498,8 @@ likwid_markerRegisterRegion(const char* regionTag)
         return -EFAULT;
     }
     TimerData timer;
+    int ret = 0;
+    uint64_t tmp = 0x0ULL;
     bstring tag = bfromcstralloc(100, regionTag);
     LikwidThreadResults* results;
     char groupSuffix[10];
@@ -505,11 +507,14 @@ likwid_markerRegisterRegion(const char* regionTag)
     bcatcstr(tag, groupSuffix);
     int cpu_id = hashTable_get(tag, &results);
     bdestroy(tag);
-//#ifdef LIKWID_USE_PERFEVENT
-    return HPMaddThread(cpu_id);
-//#else
-//    return 0;
-//#endif
+
+    // Add CPU to access layer
+    ret =  HPMaddThread(cpu_id);
+    // Perform one access to fully initialize connection to access daemon
+    uint32_t reg = counter_map[groupSet->groups[groups[0]].events[0].index].counterRegister;
+    HPMread(cpu_id, MSR_DEV, reg, &tmp);
+
+    return ret;
 }
 
 int
