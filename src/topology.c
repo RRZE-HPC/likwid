28ced910479cb4d131f760e99967352e90511e4e
Thomas Roehl <thomas.roehl@fau.de>
2018-04-19 16:36:51 +0200

    Add Intel Xeon Phi (Knights Mill) to supported architectures

--- src/topology.c
+++ src/topology.c
@@ -1124,6 +1124,7 @@ print_supportedCPUs (void)
     printf("\t%s\n",atom_goldmont_str);
     printf("\t%s\n",xeon_phi2_string);
     printf("\t%s\n",skylakeX_str);
+    printf("\t%s\n",xeon_phi3_string);
     printf("\n");
     printf("Supported AMD processors:\n");
     printf("\t%s\n",opteron_sc_str);
