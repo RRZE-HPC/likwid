270961821a08a62666c2400ea45e7537c29fc8cc
Thomas Roehl <thomas.roehl@fau.de>
2018-06-05 17:03:18 +0200

    Finalize topology in perfgroup initialization

--- src/perfgroup.c
+++ src/perfgroup.c
@@ -1738,6 +1738,7 @@ void __attribute__((constructor (103))) init_perfgroup(void)
         calc_add_int_def(name, cputopo->cacheLevels[i].lineSize, -1);
     }
     calc_add_int_def("MEM_LINESIZE", 64, -1);
+    topology_finalize();
 }
 
 void __attribute__((destructor (103))) close_perfgroup(void)
