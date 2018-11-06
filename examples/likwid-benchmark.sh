aa0f32d323152284238d038a8620e627b4a4bf66
Thomas Roehl <thomas.roehl@fau.de>
2018-11-06 10:59:46 +0100

    Some fixes to work on more platforms and support for OmniPath and InfiniBand

--- examples/likwid-benchmark.sh
+++ examples/likwid-benchmark.sh
@@ -86,84 +86,156 @@ function print_powercap_folder {
 }
 
 
-if [ $(which likwid-topology 2>/dev/null | wc -l) != "1" ]; then
-    module load likwid
-    if [ $(which likwid-topology 2>/dev/null | wc -l) != "1" ]; then
+if [ $(which likwid-topology 2>/dev/null | wc -l) -ne 1 ]; then
+    if [ -e /opt/modules/init/bash ]; then
+        source /opt/modules/init/bash
+    fi
+    module load likwid 2>/dev/null
+    if [ $(which likwid-topology 2>/dev/null | wc -l) -ne 1 ]; then
         DO_LIKWID=0
     fi
 fi
 
+
+##### Users
 header "Logged in users"
-users
-w
+ps -ef | grep "/bin/bash" | grep -v -E "^root" | cut -d ' ' -f 1 | sort -u
 
 if [ ${DO_LIKWID} -a -x likwid-pin ]; then
     header "CPUset"
     likwid-pin -p
 fi
 
+##### OS execution environment
 header "CGroups"
 echo -n "Allowed CPUs: "
 cat /sys/fs/cgroup/cpuset/cpuset.effective_cpus
 echo -n "Allowed Memory controllers: "
 cat /sys/fs/cgroup/cpuset/cpuset.effective_mems
 
+##### System topology
 header "Topology"
 if [ ${DO_LIKWID} -a -x likwid-topology ]; then
     likwid-topology
 else
     lscpu
 fi
-numactl -H
+if [ $(which numactl 2>/dev/null | wc -l ) == 1 ]; then
+    numactl -H
+fi
 
+##### Hyper Threading
+header "Hyper Threading"
+ALL_CPUS=$(grep 'processor' /proc/cpuinfo | wc -l)
+SIBLINGS=$(grep 'siblings' /proc/cpuinfo  | cut -d ':' -f 2 | head -n 1 | xargs)
+if [ $(expr ${ALL_CPUS} / ${SIBLINGS}) > 1 ]; then
+    echo "HyperThreading on"
+else
+    echo "HyperThreading off"
+fi
+
+##### CPU frequencies
 if [ ${DO_LIKWID} -a -x likwid-setFrequencies ]; then
     header "Frequencies"
     likwid-setFrequencies -p
 fi
 
+
+##### Hardware prefetchers
 if [ ${DO_LIKWID} -a -x likwid-features ]; then
     header "Prefetchers"
     likwid-features -l -c N
 fi
 
+##### System load
 header "Load"
 cat /proc/loadavg
 
-if [ ${DO_LIKWID} ]; then
-header "Performance energy bias"
-likwid-powermeter -i | grep -i bias
+##### System load
+if [ ${DO_LIKWID} -a -x likwid-powermeter ]; then
+    header "Performance energy bias"
+    likwid-powermeter -i | grep -i bias
 fi
 
+##### NUMA balancing
 header "NUMA balancing"
 echo -n "Enabled: "
 cat /proc/sys/kernel/numa_balancing
 
+##### Current memory information
 header "General memory info"
 cat /proc/meminfo
 
-header "Transparent huge pages"
-echo -n "Enabled: "
-cat /sys/kernel/mm/transparent_hugepage/enabled
-echo -n "Use zero page: "
-cat /sys/kernel/mm/transparent_hugepage/use_zero_page
+##### Transparent huge pages
+header "Transparent huge pages";
+if [ $(cat /sys/kernel/mm/transparent_hugepage/enabled) == "1" ]; then
+    echo "State: on"
+else
+    echo "State: off"
+fi
+if [ $(cat /sys/kernel/mm/transparent_hugepage/use_zero_page) == "1" ]; then
+    echo  "Use zero page: on"
+else
+    echo  "Use zero page: off"
+fi
 
-header "Hardware power limits"
-RAPL_FOLDERS=$(find /sys/devices/virtual/powercap -name "intel-rapl\:*")
-for F in ${RAPL_FOLDERS}; do print_powercap_folder $F; done
+##### Hardware power limits
+if [ -e /sys/devices/virtual/powercap ]; then
+    header "Hardware power limits"
+    RAPL_FOLDERS=$(find /sys/devices/virtual/powercap -name "intel-rapl\:*")
+    for F in ${RAPL_FOLDERS}; do print_powercap_folder $F; done
+fi
 
+##### Currently loaded modules
+if [ $(module 2>/dev/null || echo $?) -eq 0 ]; then
+    header "Modules"
+    module list
+fi
 
-header "Modules"
-module list
-
-header "Compiler"
+##### Compilers
 CC=""
 if [ $(which icc 2>/dev/null | wc -l ) == 1 ]; then
     CC=$(which icc)
 elif [ $(which gcc 2>/dev/null | wc -l ) == 1 ]; then
     CC=$(which gcc)
-fi
-$CC --version
+elif [ $(which clang 2>/dev/null | wc -l ) == 1 ]; then
+    CC=$(which clang)
+elif [ $(which pgcc 2>/dev/null | wc -l ) == 1 ]; then
+    CC=$(which pgcc)
 
+fi
+if [ ! -z $CC ]; then
+    header "C Compiler"
+    $CC --version
+fi
+
+CXX=""
+if [ $(which g++ 2>/dev/null | wc -l ) == 1 ]; then
+    CXX=$(which g++)
+elif [ $(which pgc++ 2>/dev/null | wc -l ) == 1 ]; then
+    CC=$(which pgc++)
+fi
+if [ ! -z $CXX ]; then
+    header "C++ Compiler"
+    $CXX --version
+fi
+
+FORTRAN=""
+if [ $(which ifort 2>/dev/null | wc -l ) == 1 ]; then
+    FORTRAN=$(which ifort)
+elif [ $(which gfortran 2>/dev/null | wc -l ) == 1 ]; then
+    FORTRAN=$(which gfortran)
+elif [ $(which pgf90 2>/dev/null | wc -l ) == 1 ]; then
+    FORTRAN=$(which pgf90)
+fi
+if [ ! -z $FORTRAN ]; then
+    header "Fortran Compiler"
+    $FORTRAN --version
+fi
+
+
+
+##### MPI
 header "MPI"
 if [ $(which mpiexec 2>/dev/null | wc -l ) == 1 ]; then
     mpiexec --version
@@ -175,27 +247,46 @@ else
     echo "No MPI found"
 fi
 
+##### OS distribution
 header "Operating System"
 cat /etc/*release*
+
+##### OS kernel
 header "Operating System Kernel"
 uname -a
+
+##### Hostname
 header "Hostname"
 hostname -f
 
+##### If NVIDIA tool is available, also print NVIDIA information
 if [ $(which nvidia-smi 2>/dev/null | wc -l ) == 1 ]; then
     header "Nvidia GPUs"
     nvidia-smi
 fi
 
+##### If NEC Tsubasa tool is available, also print NEC Tsubasa information
 if [ $(which veosinfo 2>/dev/null | wc -l ) == 1 ]; then
     header "NEC Tsubasa"
     veosinfo
 fi
 
+##### If InfiniBand is used
+if [ $(which ibstat 2>/dev/null | wc -l ) == 1 ]; then
+    header "InfiniBand"
+    ibstat
+fi
+
+##### If Intel OmniPath is used
+if [ $(which opafabricinfo 2>/dev/null | wc -l ) == 1 ]; then
+    header "Intel OmniPath"
+    opafabricinfo
+fi
+
+##### If an application is given, try to get version and linked libraries
 if [ $# -ge 1 ]; then
     header "Executable"
-    echo -n "Name: "
-    echo $1
+    echo "Name: $1"
     if [ $($1 --version 2>/dev/null | wc -l) -gt 0 ]; then
         echo -n "Version: "
         $1 --version
