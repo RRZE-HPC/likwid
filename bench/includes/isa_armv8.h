69f95abda1d6a8cd43a96891ac436827653101f4
Thomas Roehl <thomas.roehl@fau.de>
2019-10-31 22:14:46 +0100

    Add ISA files for different architectures

---
+++ bench/includes/isa_armv8.h
@@ -0,0 +1,190 @@
+#ifndef LIKWID_BENCH_ISA_ARMV8_H
+#define LIKWID_BENCH_ISA_ARMV8_H
+
+#define DBL_ALIGNMENT 64
+#define SGL_ALIGNMENT 64
+#define INT_ALIGNMENT 16
+
+#include <bstrlib.h>
+#include <bstrlib_helper.h>
+
+#define ARCHNAME "armv8"
+#define WORDLENGTH 4
+
+int header(struct bstrList* code, char* funcname)
+{
+    bstring glline;
+    bstring typeline;
+    bstring label;
+    if (funcname)
+    {
+        glline = bformat(".global %s", funcname);
+        typeline = bformat(".type %s, @function", funcname);
+        label = bformat("%s :", funcname);
+    }
+    else
+    {
+        glline = bformat(".global kernelfunction");
+        typeline = bformat(".type kernelfunction, @function");
+        label = bformat("kernelfunction :");
+    }
+
+
+    bstrListAddChar(code, ".cpu    generic+fp+simd");
+    bstrListAddChar(code, ".data");
+    bstrListAddChar(code, ".text");
+    bstrListAdd(code, glline);
+    bstrListAdd(code, typeline);
+    bstrListAdd(code, label);
+
+    bstrListAddChar(code, "\n");
+
+    bdestroy(glline);
+    bdestroy(typeline);
+    bdestroy(label);
+    return 0;
+}
+
+int footer(struct bstrList* code, char* funcname)
+{
+    bstring line;
+    if (funcname)
+    {
+        line = bformat(".size %s, .-%s", funcname, funcname);
+    }
+    else
+    {
+        line = bformat(".size kernelfunction, .-kernelfunction");
+    }
+    bstrListAddChar(code, ".exit:");
+    bstrListAddChar(code, "ret");
+
+    bstrListAdd(code, line);
+
+    bstrListAddChar(code, "\n");
+
+//    bstrListAddChar(code, "#if defined(__linux__) && defined(__ELF__)");
+//    bstrListAddChar(code, ".section .note.GNU-stack,"",%progbits");
+//    bstrListAddChar(code, "#endif");
+
+    bdestroy(line);
+}
+
+int loopheader(struct bstrList* code, char* loopname, int step)
+{
+    bstring line;
+    if (loopname)
+    {
+        line = bformat("%s:", loopname);
+    }
+    else
+    {
+        line = bformat("kernelfunctionloop:");
+    }
+
+    bstrListAddChar(code, "mov   GPR6, 0");
+    bstrListAdd(code, line);
+    bstrListAddChar(code, "\n");
+
+    bdestroy(line);
+    return 0;
+}
+
+int loopfooter(struct bstrList* code, char* loopname, int step)
+{
+    bstring line;
+    if (loopname)
+    {
+        line = bformat("tblt %s", loopname);
+    }
+    else
+    {
+        line = bformat("tblt kernelfunctionloop");
+    }
+    bstring bstep = bformat("add GPR6, GPR6, #%d", step);
+    bstrListAdd(code, bstep);
+    bdestroy(bstep);
+    bstrListAddChar(code, "cmp   GPR6, ARG1");
+    bstrListAdd(code, line);
+
+    bstrListAddChar(code, "\n");
+
+    bdestroy(line);
+    return 0;
+}
+
+
+static RegisterMap Registers[] = {
+    {"GPR1", "x1"},
+    {"GPR2", "x2"},
+    {"GPR3", "x3"},
+    {"GPR4", "x4"},
+    {"GPR5", "x5"},
+    {"GPR6", "x6"},
+    {"GPR7", "x7"},
+    {"GPR8", "x8"},
+    {"GPR9", "x9"},
+    {"GPR10", "x10"},
+    {"GPR11", "x11"},
+    {"GPR12", "x12"},
+    {"GPR13", "x13"},
+    {"GPR14", "x14"},
+    {"GPR15", "x15"},
+    {"GPR16", "x16"},
+    {"GPR17", "x17"},
+    {"GPR18", "x18"},
+    {"GPR19", "x19"},
+    {"GPR20", "x20"},
+    {"GPR21", "x21"},
+    {"GPR22", "x22"},
+    {"FPR1", "d0"},
+    {"FPR2", "d1"},
+    {"FPR3", "d2"},
+    {"FPR4", "d3"},
+    {"FPR5", "d4"},
+    {"FPR6", "d5"},
+    {"FPR7", "d6"},
+    {"FPR8", "d7"},
+    {"FPR9", "d8"},
+    {"FPR10", "d9"},
+    {"FPR11", "d10"},
+    {"FPR12", "d11"},
+    {"FPR13", "d12"},
+    {"FPR14", "d13"},
+    {"FPR15", "d14"},
+    {"FPR16", "d15"},
+    {"", ""},
+};
+
+static RegisterMap Arguments[] = {
+    {"ARG1", "x0"},
+    {"ARG2", "x1"},
+    {"ARG3", "x2"},
+    {"ARG4", "x3"},
+    {"ARG5", "x4"},
+    {"ARG6", "x5"},
+    {"ARG7", "x6"},
+    {"ARG8", "x7"},
+    {"ARG9", "[SPTR+32]"},
+    {"ARG10", "[SPTR+40]"},
+    {"ARG11", "[SPTR+48]"},
+    {"ARG12", "[SPTR+56]"},
+    {"ARG13", "[SPTR+64]"},
+    {"ARG14", "[SPTR+72]"},
+    {"ARG15", "[SPTR+80]"},
+    {"ARG16", "[SPTR+88]"},
+    {"ARG17", "[SPTR+96]"},
+    {"ARG18", "[SPTR+104]"},
+    {"ARG19", "[SPTR+112]"},
+    {"ARG20", "[SPTR+120]"},
+    {"ARG21", "[SPTR+128]"},
+    {"ARG22", "[SPTR+136]"},
+    {"ARG23", "[SPTR+144]"},
+    {"ARG24", "[SPTR+152]"},
+    {"", ""},
+};
+
+static RegisterMap Sptr = {"SPTR", "sp"};
+static RegisterMap Bptr = {"BPTR", "rbp"};
+
+#endif
---
+++ bench/includes/isa_x86.h
@@ -0,0 +1,176 @@
+#ifndef LIKWID_BENCH_ISA_X86_H
+#define LIKWID_BENCH_ISA_X86_H
+
+
+#include <bstrlib.h>
+#include <bstrlib_helper.h>
+
+#define ARCHNAME "x86"
+#define WORDLENGTH 4
+
+
+int header(struct bstrList* code, char* funcname)
+{
+    bstring glline;
+    bstring typeline;
+    bstring label;
+    if (funcname)
+    {
+        glline = bformat(".global %s", funcname);
+        typeline = bformat(".type %s, @function", funcname);
+        label = bformat("%s :", funcname);
+    }
+    else
+    {
+        glline = bformat(".global kernelfunction");
+        typeline = bformat(".type kernelfunction, @function");
+        label = bformat("kernelfunction :");
+    }
+
+
+    bstrListAddChar(code, ".intel_syntax noprefix");
+    bstrListAddChar(code, ".data");
+    bstrListAddChar(code, ".align 64\nSCALAR:\n.double 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0");
+    bstrListAddChar(code, ".align 64\nSSCALAR:\n.single 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0");
+    bstrListAddChar(code, ".align 64\nISCALAR:\n.int 1, 1, 1, 1, 1, 1, 1, 1");
+    bstrListAddChar(code, ".align 16\nOMM:\n.int 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15");
+    bstrListAddChar(code, ".align 16\nIOMM:\n.int 0,16,32,48,64,80,96,128,144,160,176,192,208,224,240,256");
+    bstrListAddChar(code, ".align 16\nTOMM:\n.int 0,2,4,6,16,18,20,22,32,34,36,38,48,50,52,54");
+    bstrListAddChar(code, ".text");
+    bstrListAdd(code, glline);
+    bstrListAdd(code, typeline);
+    bstrListAdd(code, label);
+
+
+    bstrListAddChar(code, "\n");
+
+    bdestroy(glline);
+    bdestroy(typeline);
+    bdestroy(label);
+    return 0;
+}
+
+int footer(struct bstrList* code, char* funcname)
+{
+    bstring line;
+    if (funcname)
+    {
+        line = bformat(".size %s, .-%s", funcname, funcname);
+    }
+    else
+    {
+        line = bformat(".size kernelfunction, .-kernelfunction");
+    }
+    bstrListAddChar(code, "pop edi");
+    bstrListAddChar(code, "pop esi");
+    bstrListAddChar(code, "pop ebx");
+    bstrListAddChar(code, "mov  esp, ebp");
+    bstrListAddChar(code, "pop ebp");
+    bstrListAddChar(code, "ret");
+
+    bstrListAdd(code, line);
+
+    bstrListAddChar(code, "\n");
+
+//    bstrListAddChar(code, "#if defined(__linux__) && defined(__ELF__)");
+//    bstrListAddChar(code, ".section .note.GNU-stack,"",%progbits");
+//    bstrListAddChar(code, "#endif");
+
+    bdestroy(line);
+}
+
+int loopheader(struct bstrList* code, char* loopname, int step)
+{
+    bstring line;
+    if (loopname)
+    {
+        line = bformat("%s:", loopname);
+    }
+    else
+    {
+        line = bformat("kernelfunctionloop:");
+    }
+
+    bstrListAddChar(code, "xor   GPR1, GPR1");
+    bstrListAddChar(code, ".align 16");
+    bstrListAdd(code, line);
+    bstrListAddChar(code, "\n");
+
+    bdestroy(line);
+    return 0;
+}
+
+int loopfooter(struct bstrList* code, char* loopname, int step)
+{
+    bstring line;
+    if (loopname)
+    {
+        line = bformat("jl %sb", loopname);
+    }
+    else
+    {
+        line = bformat("jl kernelfunctionloopb");
+    }
+    bstring bstep = bformat("add GPR1, %d", step);
+    bstrListAdd(code, bstep);
+    bdestroy(bstep);
+    bstrListAddChar(code, "cmp   GPR1, ARG1");
+    bstrListAdd(code, line);
+
+    bstrListAddChar(code, "\n");
+
+    bdestroy(line);
+    return 0;
+}
+
+
+static RegisterMap Registers[] = {
+    {"GPR1", "eax"},
+    {"GPR2", "ebx"},
+    {"GPR3", "ecx"},
+    {"GPR4", "edx"},
+    {"GPR5", "esi"},
+    {"GPR6", "edi"},
+    {"FPR1", "xmm0"},
+    {"FPR2", "xmm1"},
+    {"FPR3", "xmm2"},
+    {"FPR4", "xmm3"},
+    {"FPR5", "xmm4"},
+    {"FPR6", "xmm5"},
+    {"FPR7", "xmm6"},
+    {"FPR8", "xmm7"},
+    {"", ""},
+};
+
+static RegisterMap Arguments[] = {
+    {"ARG1", "rdi"},
+    {"ARG2", "rsi"},
+    {"ARG3", "rdx"},
+    {"ARG4", "rcx"},
+    {"ARG5", "r8"},
+    {"ARG6", "r9"},
+    {"ARG7", "[BPTR+8]"},
+    {"ARG8", "[BPTR+12]"},
+    {"ARG9", "[BPTR+16]"},
+    {"ARG10", "[BPTR+20]"},
+    {"ARG11", "[BPTR+24]"},
+    {"ARG12", "[BPTR+28]"},
+    {"ARG13", "[BPTR+32]"},
+    {"ARG14", "[BPTR+36]"},
+    {"ARG15", "[BPTR+40]"},
+    {"ARG16", "[BPTR+44]"},
+    {"ARG17", "[BPTR+48]"},
+    {"ARG18", "[BPTR+52]"},
+    {"ARG19", "[BPTR+56]"},
+    {"ARG20", "[BPTR+60]"},
+    {"ARG21", "[BPTR+64]"},
+    {"ARG22", "[BPTR+68]"},
+    {"ARG23", "[BPTR+72]"},
+    {"ARG24", "[BPTR+76]"},
+    {"", ""},
+};
+
+static RegisterMap Sptr = {"SPTR", "esp"};
+static RegisterMap Bptr = {"BPTR", "ebp"};
+
+#endif
