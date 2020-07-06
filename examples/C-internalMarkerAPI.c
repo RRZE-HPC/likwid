0c3ed359bc7db88b09b25a5d98f5bf7785a0f903
Riley Weber <rileyw13@protonmail.com>
2020-07-06 16:31:57 -0400

    added LIKWID_MARKER_RESET, improved comments removed LIKWID_MARKER_THREADINIT and added explanation added barrier hints reduced output verbosity

--- examples/C-internalMarkerAPI.c
+++ examples/C-internalMarkerAPI.c
@@ -48,8 +48,15 @@ int main(int argc, char* argv[])
      * specified in `/<LIKWID_PREFIX>/share/likwid/perfgroups/<ARCHITECTURE>/`
      * (default prefix is `/usr/local`). The final group is a custom group
      * specified with EVENT_NAME:COUNTER_NAME
+     *
+     * Be aware that the groups chosen below are supported in most but not all
+     * architectures supported by likwid. For a more compatible set of groups,
+     * see commented group set. The second, more compatible set will work for
+     * all architectures supported by likwid except nvidiagpus and Xeon Phi
+     * (KNC). 
      */
-    char groups[] = "L2|L3|FLOPS_DP|INSTR_RETIRED_ANY:FIXC0";
+    char groups[] = "L2|FLOPS_DP|INSTR_RETIRED_ANY:FIXC0";
+    // char groups[] = "BRANCH|INSTR_RETIRED_ANY:FIXC0";
 
     /* list of processors that we will use */
     char cpulist[] = "0,2,3";
@@ -104,12 +111,24 @@ int main(int argc, char* argv[])
 {
     likwid_pinThread(cpus[omp_get_thread_num()]);
 
-    /* each thread must be initialized with LIKWID_MARKER_THREADINIT */
-    LIKWID_MARKER_THREADINIT;
+    /* LIKWID_MARKER_THREADINIT was required with past versions of likwid but
+     * now is now commonly not needed and, in fact, deprecated with likwid
+     * v5.0.1
+     *
+     * It is only required if the pinning library fails and threads could get
+     * migrated. I am currently unaware of any runtime system that doesn't
+     * work. 
+     */ 
+    // LIKWID_MARKER_THREADINIT;
 
     /* registering regions is optional but strongly recommended, as it reduces
      * overhead of LIKWID_MARKER_START and prevents wrong counts in short
      * regions 
+     *
+     * There must be a barrier between registering a region and starting that
+     * region. Typically these are done in separate parallel blocks, relying on
+     * the implicit barrier at the end of the parallel block. Usually there is
+     * a parallel block for initialization and a parallel block for execution.
      */
     LIKWID_MARKER_REGISTER("Total");
     LIKWID_MARKER_REGISTER("calc_flops");
@@ -131,15 +150,19 @@ int main(int argc, char* argv[])
     double events[MAX_NUM_EVENTS];
     double time = 0;
     int count = 0;
+
+    /* variables for inspecting events */
+    int gid;
+    char * group_name, * event_name;
     
     /* loop iterators */
-    int i, k;
+    int i, k, t;
 
     /* The code that is to be measured will be run multiple times to measure
      * each group specified above. perfmon_getNumberOfGroups makes it easy to
      * run computation once for each group.
      */
-    for (k=0; k<perfmon_getNumberOfGroups(); k++)
+    for (i=0; i<perfmon_getNumberOfGroups(); i++)
     {
         /* barriers are necessary when regions are started or stopped more than
          * once in a region. In this case, put a barrier before each call to
@@ -161,6 +184,62 @@ int main(int argc, char* argv[])
 
         #pragma omp barrier
 
+        /* LIKWID_MARKER_GET can give us some information about what happened
+         * in a region. The values of each event are stored in `events` and
+         * the number of events is stored in `nr_events`. We will inspect and
+         * reset events here, in the middle of computation, but regions may
+         * also be inspected after the marker API has been closed. This is
+         * demonstrated at the end of this file.
+         */
+        LIKWID_MARKER_GET("calc_flops", &nr_events, events, &time, &count);
+
+        /* group ID will let us get group and event names */
+        gid = perfmon_getIdOfActiveGroup();
+
+        /* get group name */
+        group_name = perfmon_getGroupName(gid);
+
+        /*print basic info */
+        printf("calc_flops iteration %d, thread %d finished measuring group "
+            "%s.\n"
+            "Got %d events, runtime %f s, and call count %d\n", 
+            i, omp_get_thread_num(), group_name, nr_events, time, count);
+
+        /* only allow one thread to print so that output is less verbose. The
+         * purpose of the barrier is to prevent garbled output
+         */
+        #pragma omp barrier
+        if(omp_get_thread_num() == 0)
+        {
+            /* uncomment the for loop if you'd like to inspect all threads */
+            t=0;
+            // for (t=0; t<NUM_THREADS; t++)
+            {
+                printf("detailed event results: \n");
+                for(k=0; k<nr_events; k++){
+                    /* get event name */
+                    event_name = perfmon_getEventName(gid, k);
+                    /* print results */
+                    printf("%40s: %30f\n", event_name, 
+                        perfmon_getResult(gid, k, t)
+                    );
+                }
+            }
+            printf("\n");
+        }
+
+        /* "nr_events" must be reset because LIKWID_MARKER_GET uses it to
+         * determine the capacity of "events"
+         */
+        nr_events = MAX_NUM_EVENTS;
+
+        /* regions may be reset during execution. Since we have already
+         * inspected results of the calc_flops region, we will reset it here:
+         */
+        LIKWID_MARKER_RESET("calc_flops");
+
+        #pragma omp barrier
+
         /* this region will measure memory-heavy code */
         LIKWID_MARKER_START("copy");
 
@@ -179,6 +258,9 @@ int main(int argc, char* argv[])
          * called in a parallel region, it must be preceeded by a barrier and
          * run in something like a "#pragma omp single" block to ensure only
          * one thread runs it
+         * 
+         * regions must be switched outside of all regions (e.g. after
+         * "LIKWID_MARKER_STOP" is called for each region)
          */
         #pragma omp single
         {
@@ -186,27 +268,25 @@ int main(int argc, char* argv[])
         }
     }
 
-    /* LIKWID_MARKER_GET can give us some information about what happened in
-     * that region. The values of each event are stored in `events` and may be
-     * inspected here if desired. We choose not to, as we will inspect
-     * everything at the end.
+    /* for demonstration, we will inspect "calc_flops" region again even though
+     * it has been rest.
      */
+    if(omp_get_thread_num() == 0)
+        printf("notice that calc_flops has no calls, since we reset it.\n");
     LIKWID_MARKER_GET("calc_flops", &nr_events, events, &time, &count);
     printf("calc_flops Thread %d got %d events, runtime %f s, call count %d\n", 
         omp_get_thread_num(), nr_events, time, count);
-
-    /* "nr_events" must be reset because LIKWID_MARKER_GET uses it as the size
-     * of "events"
-     */
     nr_events = MAX_NUM_EVENTS;
 
     /* basic info on "copy" region */
+    #pragma omp barrier /* to prevent garbled output */
     LIKWID_MARKER_GET("copy", &nr_events, events, &time, &count);
     printf("copy Thread %d got %d events, runtime %f s, call count %d\n", 
         omp_get_thread_num(), nr_events, time, count);
     nr_events = MAX_NUM_EVENTS;
 
     /* basic info on "Total" region */
+    #pragma omp barrier /* to prevent garbled output */
     LIKWID_MARKER_GET("Total", &nr_events, events, &time, &count);
     printf("Total Thread %d got %d events, runtime %f s, call count %d\n", 
         omp_get_thread_num(), nr_events, time, count);
@@ -238,7 +318,15 @@ int main(int argc, char* argv[])
     /* read file output by likwid so that we can process results */
     perfmon_readMarkerFile(getenv("LIKWID_FILEPATH"));
 
-    /* get information like region name, number of events, number of metrics */
+    /* get information like region name, number of events, number of metrics.
+     * Notice that number of regions printed here is actually
+     * (num_regions*num_groups), because perfmon considers a region to be the
+     * region/group combo. In other words, each time a region is measured with
+     * a different group or event set, perfmon considers it a new region.
+     *
+     * Therefore, if we have two regions and 3 groups measured for each,
+     * perfmon_getNumberOfRegions() will return 6
+     */
     printf("Marker API measured %d regions\n", perfmon_getNumberOfRegions());
     for (i=0;i<perfmon_getNumberOfRegions();i++)
     {
@@ -250,34 +338,39 @@ int main(int argc, char* argv[])
     printf("\n");
 
     /* Print per-thread results. */
-    printf("detailed results for each thread follow:\n");
+    printf("detailed results follow. Notice that the region \"calc_flops\"\n"
+        "will not appear, as it was reset after each time it was measured.\n");
+    printf("\n");
 
     const char * result_header = "%6s : %15s : %10s : %6s : %40s : %30s \n";
     const char * result_format = "%6d : %15s : %10s : %6s : %40s : %30f \n";
     printf(result_header, "thread", "region", "group", "type", 
         "result name", "result value");
 
-    for (t = 0; t < NUM_THREADS; t++) 
+    /* uncomment the for loop if you'd like to inspect all threads */
+    t = 0;
+    // for (t = 0; t < NUM_THREADS; t++) 
     {
-        /* perfmon_getNumberOfRegions actually returns num_regions *
-         * num_groups. This is because this function considers every
-         * combination of region and group its own region.
-         */
         for (i = 0; i < perfmon_getNumberOfRegions(); i++) 
         {
+            /* returns the user-supplied region name */
             regionName = perfmon_getTagOfRegion(i);
 
             /* gid is the group ID independent of region, where as i is the ID
              * of the region/group combo
              */
             gid = perfmon_getGroupOfRegion(i);
+            /* get the name of the group measured, like "FLOPS_DP" or "L2" */
             groupName = perfmon_getGroupName(gid);
 
             /* get info for each event */
             for (k = 0; k < perfmon_getNumberOfEvents(gid); k++) 
             {
+                /* get the event name, like "INSTR_RETIRED_ANY" */
                 event_name = perfmon_getEventName(gid, k);
+                /* get the associated value */
                 event_value = perfmon_getResultOfRegionThread(i, k, t);
+
                 printf(result_format, t, regionName, groupName, "event", 
                     event_name, event_value);
             }
@@ -285,8 +378,11 @@ int main(int argc, char* argv[])
             /* get info for each metric */
             for (k = 0; k < perfmon_getNumberOfMetrics(gid); k++) 
             {
+                /* get the metric name, like "L2 bandwidth [MBytes/s]" */
                 metric_name = perfmon_getMetricName(gid, k);
+                /* get the associated value */
                 metric_value = perfmon_getMetricOfRegionThread(i, k, t);
+
                 printf(result_format, t, regionName, groupName, "metric", 
                     metric_name, metric_value);
             }
