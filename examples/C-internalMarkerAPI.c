227f771ac01d58fbdef3dd9593880e20ef6b2bf1
Riley Weber <rileyw13@protonmail.com>
2020-07-06 18:02:50 -0400

    added author, license, and usage

--- examples/C-internalMarkerAPI.c
+++ examples/C-internalMarkerAPI.c
@@ -1,3 +1,43 @@
+/*
+ * ==========================================================================
+ *
+ *      Filename:  C-internalMarkerAPI.c
+ *
+ *      Description:  Example how to use the C/C++ Marker API internally.
+ *                    Avoids the likwid-perfctr CLI for setting environment
+ *                    variables, pinning threads, and inspecting results.
+ *
+ *      Version:   1.0.0
+ *      Released:  2020-07-06
+ *
+ *      Author:   Riley Weber, rileyw13@protonmail.com
+ *      Project:  likwid
+ *
+ *
+ *      This program is free software: you can redistribute it and/or modify it
+ *      under the terms of the GNU General Public License as published by the
+ *      Free Software Foundation, either version 3 of the License, or (at your
+ *      option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful, but
+ *      WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *      General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License along
+ *      with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ==========================================================================
+ *
+ *      Usage: 
+ *
+ *      After installing likwid, change to the `examples` directory. Then, use
+ *      `make C-internalMarkerAPI` to compile and 
+ *      `make C-internalMarkerAPI-run` to run. 
+ *
+ * ==========================================================================
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
