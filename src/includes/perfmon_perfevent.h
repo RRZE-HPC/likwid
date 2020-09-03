f8ed7703410f25557cfa2bc7d44272ad1041e829
Thomas Roehl <thomas.roehl@fau.de>
2020-09-03 18:15:02 +0200

    Fix for AMD Zen/Zen2 with perf_event backend to use perf's msr device

--- src/includes/perfmon_perfevent.h
+++ src/includes/perfmon_perfevent.h
@@ -125,35 +125,7 @@ int perfmon_init_perfevent(int cpu_id)
     return 0;
 }
 
-int perf_fixed_setup(struct perf_event_attr *attr, RegisterIndex index, PerfmonEvent *event)
-{
-    int ret = -1;
-    attr->type = PERF_TYPE_HARDWARE;
-    attr->exclude_kernel = 1;
-    attr->exclude_hv = 1;
-    attr->disabled = 1;
-    attr->inherit = 1;
-    //attr->exclusive = 1;
-    if (strcmp(event->name, "INSTR_RETIRED_ANY") == 0)
-    {
-        attr->config = PERF_COUNT_HW_INSTRUCTIONS;
-        ret = 0;
-    }
-    if (strcmp(event->name, "CPU_CLK_UNHALTED_CORE") == 0)
-    {
-        attr->config = PERF_COUNT_HW_CPU_CYCLES;
-        ret = 0;
-    }
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,3,0)
-    if (strcmp(event->name, "CPU_CLK_UNHALTED_REF") == 0)
-    {
-        attr->config = PERF_COUNT_HW_REF_CPU_CYCLES;
-        ret = 0;
-    }
-#endif
 
-    return ret;
-}
 
 typedef enum {
     PERF_EVENT_INVAL_REG = 0,
@@ -269,6 +241,83 @@ uint64_t create_mask(uint32_t value, int start, int end)
         return (value << start ) & mask;
     }
     return 0x0ULL;
+}
+
+int perf_fixed_setup(struct perf_event_attr *attr, RegisterIndex index, PerfmonEvent *event)
+{
+    int ret = -1;
+    attr->type = PERF_TYPE_HARDWARE;
+    attr->disabled = 1;
+    attr->inherit = 1;
+    if (translate_types[FIXED] != NULL &&
+        strcmp(translate_types[PMC], translate_types[FIXED]) == 0)
+    {
+        attr->exclude_kernel = 1;
+        attr->exclude_hv = 1;
+        if (strcmp(event->name, "INSTR_RETIRED_ANY") == 0)
+        {
+            attr->config = PERF_COUNT_HW_INSTRUCTIONS;
+            ret = 0;
+        }
+        if (strcmp(event->name, "CPU_CLK_UNHALTED_CORE") == 0 ||
+            strcmp(event->name, "ACTUAL_CPU_CLOCK") == 0 ||
+            strcmp(event->name, "APERF") == 0)
+        {
+            attr->config = PERF_COUNT_HW_CPU_CYCLES;
+            ret = 0;
+        }
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,3,0)
+        if (strcmp(event->name, "CPU_CLK_UNHALTED_REF") == 0)
+        {
+            attr->config = PERF_COUNT_HW_REF_CPU_CYCLES;
+            ret = 0;
+        }
+#endif
+    }
+    else
+    {
+        char checkfolder[1024];
+        int perf_type = PERF_TYPE_HARDWARE;
+        int start = 0, end = 0;
+        PERF_EVENT_PMC_OPT_REGS reg = PERF_EVENT_INVAL_REG;
+        int err = snprintf(checkfolder, 1023, "%s/type", translate_types[FIXED]);
+        if (err > 0)
+        {
+            checkfolder[err] = '\0';
+        }
+        FILE* fp = fopen(checkfolder, "r");
+        if (fp != NULL)
+        {
+            ret = fread(checkfolder, sizeof(char), 1024, fp);
+            fclose(fp);
+            perf_type = atoi(checkfolder);
+            if (perf_type >= 0)
+            {
+                attr->type = perf_type;
+                getEventOptionConfig(translate_types[FIXED], EVENT_OPTION_GENERIC_CONFIG, &reg, &start, &end);
+                switch(reg)
+                {
+                    case PERF_EVENT_CONFIG_REG:
+                        attr->config |= create_mask(event->eventId, start, end);
+                        ret = 0;
+                        break;
+                    case PERF_EVENT_CONFIG1_REG:
+                        attr->config1 |= create_mask(event->eventId, start, end);
+                        ret = 0;
+                        break;
+                    case PERF_EVENT_CONFIG2_REG:
+                        attr->config2 |= create_mask(event->eventId, start, end);
+                        ret = 0;
+                        break;
+                    default:
+                        ret = -1;
+                        break;
+                }
+            }
+        }
+    }
+
+    return ret;
 }
 
 int perf_pmc_setup(struct perf_event_attr *attr, RegisterIndex index, PerfmonEvent *event)
