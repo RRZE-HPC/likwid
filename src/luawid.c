3e6df8fb668844a1fffbcb29af266591203c57bf
Thomas Roehl <Thomas.Roehl@googlemail.com>
2021-01-11 15:17:16 +0100

    Fix argument parsing for startProgram in Lua interface

--- src/luawid.c
+++ src/luawid.c
@@ -1955,7 +1955,7 @@ void
 parse(char *line, char **argv)
 {
      while (*line != '\0') {       /* if not the end of line ....... */
-          while (*line == ' ' || *line == '\t' || *line == '\n')
+          if (*line == ' ' || *line == '\t' || *line == '\n')
                *line++ = '\0';     /* replace white spaces with 0    */
           *argv++ = line;          /* save the argument position     */
           while (*line != '\0' && *line != ' ' &&
