734cb948078416f3169bbcaad46ac27cbff1d3d9
Thomas Gruber <Thomas.Roehl@googlemail.com>
2023-10-20 12:44:56 +0200

    Add functions to return support status and version of the library (#437)

---
+++ src/meta.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <likwid.h>
+
+int likwid_getMajorVersion(void)
+{
+    return (int) VERSION ;
+}
+
+int likwid_getMinorVersion(void)
+{
+    return (int) RELEASE ;
+}
+
+int likwid_getBugfixVersion(void)
+{
+    return (int) MINORVERSION ;
+}
+
+int likwid_getNvidiaSupport(void)
+{
+#ifdef LIKWID_WITH_NVMON
+    return 1;
+#else
+    return 0;
+#endif
+}
+
+int likwid_getMaxSupportedThreads(void)
+{
+    return (int) MAX_NUM_THREADS;
+}
+
+int likwid_getMaxSupportedSockets(void)
+{
+    return (int) MAX_NUM_NODES;
+}
