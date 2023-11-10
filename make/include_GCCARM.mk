7a42f1b5790b0b2eeb7d9cfc7ef6590c036f5227
Thomas Roehl <thomas.roehl@fau.de>
2023-11-10 13:02:22 +0100

    Add general ARM8 target with --march=native. See #471

---
+++ make/include_GCCARM.mk
@@ -0,0 +1,34 @@
+CC  = gcc
+FC  = gfortran
+AS  = as
+AR  = ar
+PAS = ./perl/AsmGen.pl
+GEN_PAS = ./perl/generatePas.pl
+GEN_GROUPS = ./perl/generateGroups.pl
+GEN_PMHEADER = ./perl/gen_events.pl
+
+ANSI_CFLAGS   =
+#ANSI_CFLAGS += -pedantic
+#ANSI_CFLAGS += -Wextra
+#ANSI_CFLAGS += -Wall
+
+CFLAGS   = -mcpu=native -O2 -std=c99 -Wno-format -fPIC
+#FCFLAGS  = -module ./  # ifort
+FCFLAGS  = -J ./  -fsyntax-only  #gfortran
+PASFLAGS  = ARMv8
+ASFLAGS  = 
+CPPFLAGS =
+LFLAGS   =  -pthread 
+
+SHARED_CFLAGS = -fPIC -fvisibility=hidden
+SHARED_LFLAGS = -shared -fvisibility=hidden
+
+DEFINES  = -DPAGE_ALIGNMENT=4096
+DEFINES  += -DLIKWID_MONITOR_LOCK
+DEFINES  += -DDEBUGLEV=0
+DEFINES  += -D__ARM_ARCH_8A
+
+INCLUDES =
+LIBS     = -lm -lrt
+
+
