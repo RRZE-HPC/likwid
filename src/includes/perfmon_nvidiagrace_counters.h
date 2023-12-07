fbe1e1cc844391bcb203b2723f007b4072fceaef
Thomas Roehl <thomas.roehl@fau.de>
2023-12-07 14:01:36 +0000

    Add support for SCF unit

--- src/includes/perfmon_nvidiagrace_counters.h
+++ src/includes/perfmon_nvidiagrace_counters.h
@@ -29,27 +29,35 @@
  */
 
 
-#define NUM_COUNTERS_NVIDIAGRACE 6
+#define NUM_COUNTERS_NVIDIAGRACE 13
 
-static RegisterMap nvidiagrace_counter_map[NUM_COUNTERS_A57] = {
+static RegisterMap nvidiagrace_counter_map[NUM_COUNTERS_NVIDIAGRACE] = {
     {"PMC0", PMC0, PMC, A57_PERFEVTSEL0, A57_PMC0, 0, 0, EVENT_OPTION_NONE_MASK},
     {"PMC1", PMC1, PMC, A57_PERFEVTSEL1, A57_PMC1, 0, 0, EVENT_OPTION_NONE_MASK},
     {"PMC2", PMC2, PMC, A57_PERFEVTSEL2, A57_PMC2, 0, 0, EVENT_OPTION_NONE_MASK},
     {"PMC3", PMC3, PMC, A57_PERFEVTSEL3, A57_PMC3, 0, 0, EVENT_OPTION_NONE_MASK},
     {"PMC4", PMC4, PMC, A57_PERFEVTSEL4, A57_PMC4, 0, 0, EVENT_OPTION_NONE_MASK},
     {"PMC5", PMC5, PMC, A57_PERFEVTSEL5, A57_PMC5, 0, 0, EVENT_OPTION_NONE_MASK},
+    {"SCFFIX", PMC6, QBOX0, 0, 0, 0, 0, EVENT_OPTION_NONE_MASK},
+    {"SCF0", PMC7, QBOX0, 0, 0, 0, 0, EVENT_OPTION_NONE_MASK},
+    {"SCF1", PMC8, QBOX0, 0, 0, 0, 0, EVENT_OPTION_NONE_MASK},
+    {"SCF2", PMC9, QBOX0, 0, 0, 0, 0, EVENT_OPTION_NONE_MASK},
+    {"SCF3", PMC10, QBOX0, 0, 0, 0, 0, EVENT_OPTION_NONE_MASK},
+    {"SCF4", PMC11, QBOX0, 0, 0, 0, 0, EVENT_OPTION_NONE_MASK},
+    {"SCF5", PMC12, QBOX0, 0, 0, 0, 0, EVENT_OPTION_NONE_MASK},
 };
 
 static BoxMap nvidiagrace_box_map[NUM_UNITS] = {
     [PMC] = {A57_PERF_CONTROL_CTRL, A57_OVERFLOW_STATUS, A57_OVERFLOW_FLAGS, 0, 0, 0, 32},
+    [QBOX0] = {0, 0, 0, 0, 0, 0, 32},
 };
 
 static char* nvidiagrace_translate_types[NUM_UNITS] = {
     [PMC] = "/sys/bus/event_source/devices/armv8_pmuv3_0",
-    [QBOX0] = "/sys/bus/event_source/devices/nvidia_cnvlink_pmu_0",
+    [PBOX3] = "/sys/bus/event_source/devices/nvidia_cnvlink_pmu_0",
     [PBOX0] = "/sys/bus/event_source/devices/nvidia_nvlink_c2c0_pmu_0",
     [PBOX1] = "/sys/bus/event_source/devices/nvidia_nvlink_c2c1_pmu_0",
     [PBOX2] = "/sys/bus/event_source/devices/nvidia_pcie_pmu_0",
-    [MBOX0] = "/sys/bus/event_source/devices/nvidia_scf_pmu_0"
+    [QBOX0] = "/sys/bus/event_source/devices/nvidia_scf_pmu_0"
 };
 
