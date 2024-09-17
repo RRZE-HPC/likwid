ebfd73b5565e47fe75fcbe95caf9c3f203286676
Thomas Roehl <thomas.roehl@fau.de>
2024-09-17 14:46:13 +0000

    Add a space in macro for ancient compilers

--- src/includes/types.h
+++ src/includes/types.h
@@ -77,7 +77,7 @@ typedef struct {
 #define FOR_EACH(macro, ...)                                                  \
       __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
 #define FOR_EACH_HELPER(macro, a1, ...)                                       \
-      macro(a1)__VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+      macro(a1) __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
 #define FOR_EACH_AGAIN() FOR_EACH_HELPER
 #define STRINGIFY(x) #x
 #define TOSTRING(...) FOR_EACH(STRINGIFY, __VA_ARGS__)
