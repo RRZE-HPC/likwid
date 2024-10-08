ba90420a4faf6d22bac399cc683cd4e96f5a380c
Michael Panzlaff <michael.panzlaff@fau.de>
2024-10-08 14:07:50 +0200

    Avoid namespace clashes for string <-> uint64 functions

--- src/includes/sysFeatures_common.h
+++ src/includes/sysFeatures_common.h
@@ -6,8 +6,8 @@
 int register_features(_SysFeatureList *features, const _SysFeatureList* in);
 int sysFeatures_init_generic(const _HWArchFeatures* infeatures, _SysFeatureList *list);
 
-int _uint64_to_string(uint64_t value, char** str);
-int _string_to_uint64(const char* str, uint64_t* value);
+int sysFeatures_uint64_to_string(uint64_t value, char** str);
+int sysFeatures_string_to_uint64(const char* str, uint64_t* value);
 int sysFeatures_double_to_string(double value, char **str);
 int sysFeatures_string_to_double(const char* str, double *value);
 
