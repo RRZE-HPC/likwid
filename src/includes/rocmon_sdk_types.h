82c33e9de3774fece73d8d455ee6aef4badac945
Thomas Roehl <thomas.roehl@fau.de>
2024-10-08 21:37:26 +0200

    Add skeleton for rocmon sdk

---
+++ src/includes/rocmon_sdk_types.h
@@ -0,0 +1,35 @@
+/*
+ * =======================================================================================
+ *
+ *      Filename:  rocmon_sdk_types.h
+ *
+ *      Description:  Header File of rocmon sdk module for ROCM >= 6.2
+ *
+ *      Version:   <VERSION>
+ *      Released:  <DATE>
+ *
+ *      Author:   Thomas Gruber (tg), thomas.gruber@googlemail.com
+ *      Project:  likwid
+ *
+ *      Copyright (C) 2019 RRZE, University Erlangen-Nuremberg
+ *
+ *      This program is free software: you can redistribute it and/or modify it under
+ *      the terms of the GNU General Public License as published by the Free Software
+ *      Foundation, either version 3 of the License, or (at your option) any later
+ *      version.
+ *
+ *      This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *      WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *      PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License along with
+ *      this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * =======================================================================================
+ */
+#ifndef LIKWID_ROCMON_SDK_TYPES_H
+#define LIKWID_ROCMON_SDK_TYPES_H
+
+
+
+#endif /* LIKWID_ROCMON_SDK_TYPES_H */
