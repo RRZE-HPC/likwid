ff52fdf7daf608121130a68da0999b9aa903f9a4
Michael Panzlaff <michael.panzlaff@fau.de>
2024-10-09 18:47:14 +0200

    Clearnup code in sysFeatures_intel_uncorefreq

--- src/includes/sysFeatures_intel_uncorefreq.h
+++ src/includes/sysFeatures_intel_uncorefreq.h
@@ -2,7 +2,7 @@
 #define HWFEATURES_X86_INTEL_UNCOREFREQ_H
 
 
-int intel_uncorefreq_test();
+int intel_uncorefreq_test(void);
 int intel_uncore_cur_freq_getter(LikwidDevice_t device, char** value);
 int intel_uncore_min_freq_getter(LikwidDevice_t device, char** value);
 int intel_uncore_max_freq_getter(LikwidDevice_t device, char** value);
@@ -10,9 +10,9 @@ int intel_uncore_max_freq_getter(LikwidDevice_t device, char** value);
 
 #define MAX_INTEL_UNCOREFREQ_FEATURES 3
 static _SysFeature intel_uncorefreq_features[] = {
-    {"cur_uncore_freq", "uncore_freq", "Current Uncore frequency", intel_uncore_cur_freq_getter, NULL, DEVICE_TYPE_SOCKET},
-    {"min_uncore_freq", "uncore_freq", "Minimum Uncore frequency", intel_uncore_min_freq_getter, NULL, DEVICE_TYPE_SOCKET},
-    {"max_uncore_freq", "uncore_freq", "Maximal Uncore frequency", intel_uncore_max_freq_getter, NULL, DEVICE_TYPE_SOCKET},
+    {"cur_uncore_freq", "uncore_freq", "Current Uncore frequency", intel_uncore_cur_freq_getter, NULL, DEVICE_TYPE_SOCKET, NULL, "MHz"},
+    {"min_uncore_freq", "uncore_freq", "Minimum Uncore frequency", intel_uncore_min_freq_getter, NULL, DEVICE_TYPE_SOCKET, NULL, "MHz"},
+    {"max_uncore_freq", "uncore_freq", "Maximal Uncore frequency", intel_uncore_max_freq_getter, NULL, DEVICE_TYPE_SOCKET, NULL, "MHz"},
 };
 
 static _SysFeatureList intel_uncorefreq_feature_list = {
