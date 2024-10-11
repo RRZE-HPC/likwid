816b5ab1958dd91a6236130e1521903584230a2e
Michael Panzlaff <michael.panzlaff@fau.de>
2024-10-11 16:44:56 +0200

    Cleanup out of place register defines in sysFeatures_amd

--- src/sysFeatures_amd.c
+++ src/sysFeatures_amd.c
@@ -22,7 +22,6 @@ int likwid_sysft_init_x86_amd(_SysFeatureList* out)
     return 0;
 }
 
-#define MSR_AMD19_PREFETCH_CONTROL 0xC0000108
 static int amd_cpu_prefetch_control_getter(const LikwidDevice_t device, int bitoffset, int width, bool invert, char** value)
 {
     uint64_t msrData = 0;
@@ -111,11 +110,10 @@ int amd_cpu_up_down_setter(const LikwidDevice_t device, const char* value)
     return amd_cpu_prefetch_control_setter(device, 5, 1, true, value);
 }
 
-#define AMD_K19_SPEC_CONTROL 0x00000048
 int amd_cpu_spec_control_getter(const LikwidDevice_t device, int bitoffset, int width, bool invert, char** value)
 {
     uint64_t msrData = 0;
-    int err = HPMread(device->id.simple.id, MSR_DEV, AMD_K19_SPEC_CONTROL, &msrData);
+    int err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD19_SPEC_CTRL, &msrData);
     if (err < 0)
     {
         return err;
@@ -137,7 +135,7 @@ int amd_cpu_spec_control_setter(const LikwidDevice_t device, int bitoffset, int 
         return err;
     }
     uint64_t msrData = 0;
-    err = HPMread(device->id.simple.id, MSR_DEV, AMD_K19_SPEC_CONTROL, &msrData);
+    err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD19_SPEC_CTRL, &msrData);
     if (err < 0)
     {
         return err;
@@ -147,7 +145,7 @@ int amd_cpu_spec_control_setter(const LikwidDevice_t device, int bitoffset, int 
         control = !control;
     }
     field64set(&msrData, bitoffset, width, control);
-    return HPMwrite(device->id.simple.id, MSR_DEV, AMD_K19_SPEC_CONTROL, msrData);
+    return HPMwrite(device->id.simple.id, MSR_DEV, MSR_AMD19_SPEC_CTRL, msrData);
 }
 
 int amd_cpu_spec_ibrs_getter(const LikwidDevice_t device, char** value)
@@ -190,7 +188,6 @@ int amd_cpu_spec_pfsd_setter(const LikwidDevice_t device, const char* value)
     return amd_cpu_spec_control_setter(device, 7, 1, true, value);
 }
 
-#define AMD_K19_L1D_FLUSH_REGISTER 0x0000010B
 int amd_cpu_flush_l1(const LikwidDevice_t device, const char* value)
 {
     uint64_t flush;
@@ -199,14 +196,13 @@ int amd_cpu_flush_l1(const LikwidDevice_t device, const char* value)
     {
         return err;
     }
-    return HPMwrite(device->id.simple.id, MSR_DEV, AMD_K19_L1D_FLUSH_REGISTER, flush & 0x1);
+    return HPMwrite(device->id.simple.id, MSR_DEV, MSR_AMD19_L1D_FLUSH_REGISTER, flush & 0x1);
 }
 
-#define AMD_K19_HWCONFIG_REGISTER 0xC0010015
 int amd_cpu_hwconfig_getter(const LikwidDevice_t device, int bitoffset, int width, bool invert, char** value)
 {
     uint64_t msrData = 0x0;
-    int err = HPMread(device->id.simple.id, MSR_DEV, AMD_K19_HWCONFIG_REGISTER, &msrData);
+    int err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD17_HW_CONFIG, &msrData);
     if (err < 0)
     {
         return err;
@@ -228,7 +224,7 @@ int amd_cpu_hwconfig_setter(const LikwidDevice_t device, int bitoffset, int widt
         return err;
     }
     uint64_t msrData = 0;
-    err = HPMread(device->id.simple.id, MSR_DEV, AMD_K19_HWCONFIG_REGISTER, &msrData);
+    err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD17_HW_CONFIG, &msrData);
     if (err < 0)
     {
         return err;
@@ -238,7 +234,7 @@ int amd_cpu_hwconfig_setter(const LikwidDevice_t device, int bitoffset, int widt
         hwconfig = !hwconfig;
     }
     field64set(&msrData, bitoffset, width, hwconfig);
-    return HPMwrite(device->id.simple.id, MSR_DEV, AMD_K19_HWCONFIG_REGISTER, msrData);
+    return HPMwrite(device->id.simple.id, MSR_DEV, MSR_AMD17_HW_CONFIG, msrData);
 }
 
 int amd_cpu_hwconfig_cpddis_getter(const LikwidDevice_t device, char** value)
