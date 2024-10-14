64eda4373fe05308206989b05901adaee8be5644
Michael Panzlaff <michael.panzlaff@fau.de>
2024-10-14 17:54:40 +0200

    Make more functions static in sysFeatures_intel_uncorefreq.c

--- src/sysFeatures_intel_uncorefreq.c
+++ src/sysFeatures_intel_uncorefreq.c
@@ -12,7 +12,7 @@
 #include <registers.h>
 #include <bitUtil.h>
 
-int intel_uncorefreq_test(void)
+static int intel_uncorefreq_test(void)
 {
     const int retval1 = likwid_sysft_foreach_socket_testmsr(MSR_UNCORE_FREQ);
     if (retval1 < 0)
@@ -27,7 +27,7 @@ int intel_uncorefreq_test(void)
     return retval1 && retval2;
 }
 
-int intel_uncore_cur_freq_getter(const LikwidDevice_t device, char** value)
+static int intel_uncore_cur_freq_getter(const LikwidDevice_t device, char** value)
 {
     if ((!device) || (!value) || (device->type != DEVICE_TYPE_SOCKET))
     {
@@ -42,7 +42,7 @@ int intel_uncore_cur_freq_getter(const LikwidDevice_t device, char** value)
     return likwid_sysft_uint64_to_string(field64(msrData, 0, 8) * 100, value);
 }
 
-int intel_uncore_min_freq_getter(const LikwidDevice_t device, char** value)
+static int intel_uncore_min_freq_getter(const LikwidDevice_t device, char** value)
 {
     if ((!device) || (!value) || (device->type != DEVICE_TYPE_SOCKET))
     {
@@ -57,7 +57,7 @@ int intel_uncore_min_freq_getter(const LikwidDevice_t device, char** value)
     return likwid_sysft_uint64_to_string(field64(msrData, 8, 8) * 100, value);
 }
 
-int intel_uncore_max_freq_getter(const LikwidDevice_t device, char** value)
+static int intel_uncore_max_freq_getter(const LikwidDevice_t device, char** value)
 {
     if ((!device) || (!value) || (device->type != DEVICE_TYPE_SOCKET))
     {
