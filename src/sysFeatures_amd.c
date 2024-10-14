602e908fabcad41dbeb20fb519f58f506d697aae
Michael Panzlaff <michael.panzlaff@fau.de>
2024-10-14 13:03:39 +0200

    Cleanup unnecessary getters/setters in sysFeatures_amd

--- src/sysFeatures_amd.c
+++ src/sysFeatures_amd.c
@@ -28,92 +28,54 @@ int likwid_sysft_init_x86_amd(_SysFeatureList* out)
     return 0;
 }
 
-static int amd_cpu_prefetch_control_getter(const LikwidDevice_t device, int bitoffset, int width, bool invert, char** value)
-{
-    uint64_t msrData = 0;
-    int err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD19_PREFETCH_CONTROL, &msrData);
-    if (err < 0)
-    {
-        return err;
-    }
-    uint64_t val = field64(msrData, bitoffset, width);
-    if (invert)
-    {
-        val = !val;
-    }
-    return likwid_sysft_uint64_to_string(val, value);
-}
-
-static int amd_cpu_prefetch_control_setter(const LikwidDevice_t device, int bitoffset, int width, bool invert, const char* value)
-{
-    uint64_t control;
-    int err = likwid_sysft_string_to_uint64(value, &control);
-    if (err < 0)
-    {
-        return err;
-    }
-    uint64_t msrData = 0;
-    err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD19_PREFETCH_CONTROL, &msrData);
-    if (err < 0)
-    {
-        return err;
-    }
-    if (invert)
-    {
-        control = !control;
-    }
-    field64set(&msrData, bitoffset, width, control);
-    return HPMwrite(device->id.simple.id, MSR_DEV, MSR_AMD19_PREFETCH_CONTROL, msrData);
-}
-
 int amd_cpu_l1_stream_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_prefetch_control_getter(device, 0, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_PREFETCH_CONTROL, 0, true, value);
 }
 
 int amd_cpu_l1_stream_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_prefetch_control_setter(device, 0, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_PREFETCH_CONTROL, 0, true, value);
 }
 
 int amd_cpu_l1_stride_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_prefetch_control_getter(device, 1, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_PREFETCH_CONTROL, 1, true, value);
 }
 
 int amd_cpu_l1_stride_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_prefetch_control_setter(device, 1, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_PREFETCH_CONTROL, 1, true, value);
 }
 
 int amd_cpu_l1_region_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_prefetch_control_getter(device, 2, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_PREFETCH_CONTROL, 2, true, value);
 }
 
 int amd_cpu_l1_region_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_prefetch_control_setter(device, 2, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_PREFETCH_CONTROL, 2, true, value);
 }
 
 int amd_cpu_l2_stream_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_prefetch_control_getter(device, 3, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_PREFETCH_CONTROL, 3, true, value);
 }
 
 int amd_cpu_l2_stream_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_prefetch_control_setter(device, 3, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_PREFETCH_CONTROL, 3, true, value);
 }
 
 int amd_cpu_up_down_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_prefetch_control_getter(device, 5, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_PREFETCH_CONTROL, 5, true, value);
 }
 
 int amd_cpu_up_down_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_prefetch_control_setter(device, 5, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_PREFETCH_CONTROL, 5, true, value);
 }
 
 static _SysFeature amd_k19_cpu_prefetch_features[] = {
@@ -129,82 +91,44 @@ static const _SysFeatureList amd_k19_cpu_prefetch_feature_list = {
     .features = amd_k19_cpu_prefetch_features,
 };
 
-int amd_cpu_spec_control_getter(const LikwidDevice_t device, int bitoffset, int width, bool invert, char** value)
-{
-    uint64_t msrData = 0;
-    int err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD19_SPEC_CTRL, &msrData);
-    if (err < 0)
-    {
-        return err;
-    }
-    uint64_t control = field64(msrData, bitoffset, width);
-    if (invert)
-    {
-        control = !control;
-    }
-    return likwid_sysft_uint64_to_string(control, value);
-}
-
-int amd_cpu_spec_control_setter(const LikwidDevice_t device, int bitoffset, int width, bool invert, const char* value)
-{
-    uint64_t control = 0x0;
-    int err = likwid_sysft_string_to_uint64(value, &control);
-    if (err < 0)
-    {
-        return err;
-    }
-    uint64_t msrData = 0;
-    err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD19_SPEC_CTRL, &msrData);
-    if (err < 0)
-    {
-        return err;
-    }
-    if (invert)
-    {
-        control = !control;
-    }
-    field64set(&msrData, bitoffset, width, control);
-    return HPMwrite(device->id.simple.id, MSR_DEV, MSR_AMD19_SPEC_CTRL, msrData);
-}
-
 int amd_cpu_spec_ibrs_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_spec_control_getter(device, 0, 1, false, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_SPEC_CTRL, 0, false, value);
 }
 
 int amd_cpu_spec_ibrs_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_spec_control_setter(device, 0, 1, false, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_SPEC_CTRL, 0, false, value);
 }
 
 int amd_cpu_spec_stibp_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_spec_control_getter(device, 1, 1, false, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_SPEC_CTRL, 1, false, value);
 }
 
 int amd_cpu_spec_stibp_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_spec_control_setter(device, 1, 1, false, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_SPEC_CTRL, 1, false, value);
 }
 
 int amd_cpu_spec_ssbd_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_spec_control_getter(device, 2, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_SPEC_CTRL, 2, true, value);
 }
 
 int amd_cpu_spec_ssbd_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_spec_control_setter(device, 2, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_SPEC_CTRL, 2, true, value);
 }
 
 int amd_cpu_spec_pfsd_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_spec_control_getter(device, 7, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD19_SPEC_CTRL, 7, true, value);
 }
 
 int amd_cpu_spec_pfsd_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_spec_control_setter(device, 7, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD19_SPEC_CTRL, 7, true, value);
 }
 
 static _SysFeature amd_k19_cpu_speculation_features[] = {
@@ -239,52 +163,14 @@ static const _SysFeatureList amd_k19_cpu_l1dflush_feature_list = {
     .features = amd_k19_cpu_l1dflush_features,
 };
 
-int amd_cpu_hwconfig_getter(const LikwidDevice_t device, int bitoffset, int width, bool invert, char** value)
-{
-    uint64_t msrData = 0x0;
-    int err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD17_HW_CONFIG, &msrData);
-    if (err < 0)
-    {
-        return err;
-    }
-    uint64_t hwconfig = field64(msrData, bitoffset, width);
-    if (invert)
-    {
-        hwconfig = !hwconfig;
-    }
-    return likwid_sysft_uint64_to_string(hwconfig, value);
-}
-
-int amd_cpu_hwconfig_setter(const LikwidDevice_t device, int bitoffset, int width, bool invert, const char* value)
-{
-    uint64_t hwconfig;
-    int err = likwid_sysft_string_to_uint64(value, &hwconfig);
-    if (err < 0)
-    {
-        return err;
-    }
-    uint64_t msrData = 0;
-    err = HPMread(device->id.simple.id, MSR_DEV, MSR_AMD17_HW_CONFIG, &msrData);
-    if (err < 0)
-    {
-        return err;
-    }
-    if (invert)
-    {
-        hwconfig = !hwconfig;
-    }
-    field64set(&msrData, bitoffset, width, hwconfig);
-    return HPMwrite(device->id.simple.id, MSR_DEV, MSR_AMD17_HW_CONFIG, msrData);
-}
-
 int amd_cpu_hwconfig_cpddis_getter(const LikwidDevice_t device, char** value)
 {
-    return amd_cpu_hwconfig_getter(device, 25, 1, true, value);
+    return likwid_sysft_readmsr_bit_to_string(device, MSR_AMD17_HW_CONFIG, 25, true, value);
 }
 
 int amd_cpu_hwconfig_cpddis_setter(const LikwidDevice_t device, const char* value)
 {
-    return amd_cpu_hwconfig_setter(device, 25, 1, true, value);
+    return likwid_sysft_writemsr_bit_from_string(device, MSR_AMD17_HW_CONFIG, 25, true, value);
 }
 
 static _SysFeature amd_k19_cpu_hwconfig_features[] = {
