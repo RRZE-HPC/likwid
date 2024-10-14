32f8cad31d2c1db1bdc77fb1b7248c1411a9cf67
Michael Panzlaff <michael.panzlaff@fau.de>
2024-10-14 17:50:47 +0200

    Make more functions static in sysFeatures_intel_spec_ctrl.c

--- src/sysFeatures_intel_spec_ctrl.c
+++ src/sysFeatures_intel_spec_ctrl.c
@@ -10,86 +10,86 @@
 #include <bitUtil.h>
 #include <registers.h>
 
-int intel_cpu_spec_ibrs_tester(void)
+static int intel_cpu_spec_ibrs_tester(void)
 {
     unsigned eax = 0x07, ebx = 0, ecx = 0, edx = 0;
     CPUID(eax, ebx, ecx, edx);
     return testBit(edx, 26);
 }
 
-int intel_cpu_spec_ibrs_getter(const LikwidDevice_t device, char** value)
+static int intel_cpu_spec_ibrs_getter(const LikwidDevice_t device, char** value)
 {
     return likwid_sysft_readmsr_bit_to_string(device, MSR_IA32_SPEC_CTRL, 0, false, value);
 }
 
-int intel_cpu_spec_stibp_tester(void)
+static int intel_cpu_spec_stibp_tester(void)
 {
     unsigned eax = 0x07, ebx = 0, ecx = 0, edx = 0;
     CPUID(eax, ebx, ecx, edx);
     return testBit(edx, 27);
 }
 
-int intel_cpu_spec_stibp_getter(const LikwidDevice_t device, char** value)
+static int intel_cpu_spec_stibp_getter(const LikwidDevice_t device, char** value)
 {
     return likwid_sysft_readmsr_bit_to_string(device, MSR_IA32_SPEC_CTRL, 1, false, value);
 }
 
-int intel_cpu_spec_ssbd_tester(void)
+static int intel_cpu_spec_ssbd_tester(void)
 {
     unsigned eax = 0x07, ebx = 0, ecx = 0, edx = 0;
     CPUID(eax, ebx, ecx, edx);
     return testBit(edx, 31);
 }
 
-int intel_cpu_spec_ssbd_getter(const LikwidDevice_t device, char** value)
+static int intel_cpu_spec_ssbd_getter(const LikwidDevice_t device, char** value)
 {
     return likwid_sysft_readmsr_bit_to_string(device, MSR_IA32_SPEC_CTRL, 2, true, value);
 }
 
-int intel_cpu_spec_ipred_dis_tester(void)
+static int intel_cpu_spec_ipred_dis_tester(void)
 {
     unsigned eax = 0x07, ebx = 0, ecx = 0x02, edx = 0;
     CPUID(eax, ebx, ecx, edx);
     return testBit(edx, 1);
 }
 
-int intel_cpu_spec_ipred_dis_getter(const LikwidDevice_t device, char** value)
+static int intel_cpu_spec_ipred_dis_getter(const LikwidDevice_t device, char** value)
 {
     return likwid_sysft_readmsr_bit_to_string(device, MSR_IA32_SPEC_CTRL, 3, false, value);
 }
 
-int intel_cpu_spec_rrsba_dis_tester(void)
+static int intel_cpu_spec_rrsba_dis_tester(void)
 {
     unsigned eax = 0x07, ebx = 0, ecx = 0x02, edx = 0;
     CPUID(eax, ebx, ecx, edx);
     return testBit(edx, 2);
 }
 
-int intel_cpu_spec_rrsba_dis_getter(const LikwidDevice_t device, char** value)
+static int intel_cpu_spec_rrsba_dis_getter(const LikwidDevice_t device, char** value)
 {
     return likwid_sysft_readmsr_bit_to_string(device, MSR_IA32_SPEC_CTRL, 5, true, value);
 }
 
-int intel_cpu_spec_psfd_tester(void)
+static int intel_cpu_spec_psfd_tester(void)
 {
     unsigned eax = 0x07, ebx = 0, edx = 0x02, ecx = 0;
     CPUID(eax, ebx, ecx, edx);
     return testBit(edx, 0);
 }
 
-int intel_cpu_spec_psfd_getter(const LikwidDevice_t device, char** value)
+static int intel_cpu_spec_psfd_getter(const LikwidDevice_t device, char** value)
 {
     return likwid_sysft_readmsr_bit_to_string(device, MSR_IA32_SPEC_CTRL, 7, true, value);
 }
 
-int intel_cpu_spec_ddpd_tester(void)
+static int intel_cpu_spec_ddpd_tester(void)
 {
     unsigned eax = 0x07, ebx = 0, ecx = 0x02, edx = 0;
     CPUID(eax, ebx, ecx, edx);
     return testBit(edx, 3);
 }
 
-int intel_cpu_spec_ddpd_getter(const LikwidDevice_t device, char** value)
+static int intel_cpu_spec_ddpd_getter(const LikwidDevice_t device, char** value)
 {
     return likwid_sysft_readmsr_bit_to_string(device, MSR_IA32_SPEC_CTRL, 8, true, value);
 }
