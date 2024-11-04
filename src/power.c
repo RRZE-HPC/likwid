49eb72d732090f31b399cce5e2b52ffd5fd2019f
Emily Kooistra <a66@nikhef.nl>
2024-11-04 12:33:04 +0100

    Add support for power monitoring on zen4 Bergamo

--- src/power.c
+++ src/power.c
@@ -217,6 +217,7 @@ power_init(int cpuId)
                 case ZEN4_RYZEN:
                 case ZEN4_RYZEN2:
                 case ZEN4_EPYC:
+		case ZEN4_EPYC_BERGAMO:
                 case ZEN4_RYZEN_PRO:
                     cpuid_info.turbo = 0;
                     power_info.hasRAPL = 1;
@@ -514,7 +515,7 @@ power_init(int cpuId)
             }
         }
 
-        if (cpuid_info.family == ZEN3_FAMILY && (cpuid_info.model == ZEN4_RYZEN || cpuid_info.model == ZEN4_RYZEN2 || cpuid_info.model == ZEN4_EPYC))
+        if (cpuid_info.family == ZEN3_FAMILY && (cpuid_info.model == ZEN4_RYZEN || cpuid_info.model == ZEN4_RYZEN2 || cpuid_info.model == ZEN4_EPYC || cpuid_info.model == ZEN4_EPYC_BERGAMO))
         {
             err = HPMread(cpuId, MSR_DEV, MSR_AMD19_RAPL_L3_UNIT, &flags);
             if (err == 0)
