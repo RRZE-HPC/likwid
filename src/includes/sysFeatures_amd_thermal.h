1a76091bfb6e2e882af642646575df21ef468eef
Michael Panzlaff <michael.panzlaff@fau.de>
2024-12-04 16:26:49 +0100

    Add temperature reading for AMD to sysfeatures

---
+++ src/includes/sysFeatures_amd_thermal.h
@@ -0,0 +1,8 @@
+#ifndef SYSFEATURES_AMD_THERMAL_H
+#define SYSFEATURES_AMD_THERMAL_H
+
+#include <sysFeatures_types.h>
+
+extern const _SysFeatureList likwid_sysft_amd_k10_cpu_thermal_feature_list;
+
+#endif //SYSFEATURES_AMD_THERMAL_H
