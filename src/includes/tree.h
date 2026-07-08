0dda52255393ffbd3e0ef15a5df32babe7441181
Michael Panzlaff <michael.panzlaff@fau.de>
2026-07-08 16:11:32 +0200

    clang-format: src/includes/tree.h

--- src/includes/tree.h
+++ src/includes/tree.h
@@ -33,15 +33,15 @@
 
 #include <types.h>
 
-extern void tree_init(TreeNode** root, int id);
-extern void tree_print(TreeNode* nodePtr);
-extern void tree_destroy(TreeNode* nodePtr);
-extern void tree_insertNode(TreeNode* nodePtr, int id);
-extern int tree_nodeExists(TreeNode* nodePtr, int id);
-extern int tree_countChildren(TreeNode* nodePtr);
-extern void tree_sort(TreeNode* root);
-extern TreeNode* tree_getNode(TreeNode* nodePtr, int id);
-extern TreeNode* tree_getChildNode(TreeNode* nodePtr);
-extern TreeNode* tree_getNextNode(TreeNode* nodePtr);
+extern void tree_init(TreeNode **root, int id);
+extern void tree_print(TreeNode *nodePtr);
+extern void tree_destroy(TreeNode *nodePtr);
+extern void tree_insertNode(TreeNode *nodePtr, int id);
+extern int tree_nodeExists(TreeNode *nodePtr, int id);
+extern int tree_countChildren(TreeNode *nodePtr);
+extern void tree_sort(TreeNode *root);
+extern TreeNode *tree_getNode(TreeNode *nodePtr, int id);
+extern TreeNode *tree_getChildNode(TreeNode *nodePtr);
+extern TreeNode *tree_getNextNode(TreeNode *nodePtr);
 
 #endif /*TREE_H*/
